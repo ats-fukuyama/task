8fdf9248a144eb179ca6f8bcb2ad7c3c9e4a82b9
Atsushi Fukuyama <fukuyama@nucleng.kyoto-u.ac.jp>
2003-10-13 21:12:31 +0000

    repair eqdata output

--- lib/libbnd.f
+++ lib/libbnd.f
@@ -24,73 +24,74 @@ C
       LHM = LH-1
       NM  = N -1
 C
-      DO 30 K = 1 , LHM
+      DO K = 1 , LHM
          LHMK = LH-K
          NPMK = N+1-K
-         DO 30 I = 1 , LHMK
+         DO I = 1 , LHMK
             LPMI = L+1-I
-            DO 40 J = 2 , L
+            DO J = 2 , L
                A( J-1 , K ) = A( J , K )
-   40       CONTINUE
+            ENDDO
             A( L    , K    ) = 0.D0
             A( LPMI , NPMK ) = 0.D0
-   30 CONTINUE
+         ENDDO
+      ENDDO
 C
-      DO 50 I = 1 , NM
+      DO I = 1 , NM
          IPIVOT = I
          IP     = I+1
          ABS2   = DABS( A(1,IPIVOT) )
-         DO 60 K = IP , LH
+         DO K = IP , LH
             ABS1 = DABS( A(1,K) )
             IF( ABS1 .GT. ABS2 ) THEN
                 IPIVOT = K
                 ABS2 = ABS1
             ENDIF
-   60    CONTINUE
+         ENDDO
 C
          IF( ABS2 .LT. EPS ) GO TO 9002
          IF( IPIVOT .NE. I ) THEN
             TEMP        = X( I      )
             X( I      ) = X( IPIVOT )
             X( IPIVOT ) = TEMP
-            DO 90 J = 1 , L
+            DO J = 1 , L
                TEMP            = A( J , I      )
                A( J , I      ) = A( J , IPIVOT )
                A( J , IPIVOT ) = TEMP
-   90       CONTINUE
+            ENDDO
          END IF
 C
          TEMP   = 1.D0   / A( 1 , I )
          X( I ) = X( I ) * TEMP
 C
-         DO 120 J = 2 , L
+         DO J = 2 , L
             A( J , I ) = A( J , I ) * TEMP
-  120    CONTINUE
+         ENDDO
 C
-         DO 130 K = IP , LH
+         DO K = IP , LH
             TEMP   = A( 1 , K )
             X( K ) = X( K ) - X( I ) * TEMP
-            DO 140 J = 2 , L
+            DO J = 2 , L
                A( J-1 , K ) = A( J , K ) - A( J , I ) * TEMP
-  140       CONTINUE
+            ENDDO
 C
             A( L , K ) = 0.D0
-  130    CONTINUE
+         ENDDO
          IF( LH .LT. N ) LH = LH + 1
-   50 CONTINUE
+      ENDDO
 C
       IF( DABS(A(1,N)) .LT. EPS ) GO TO 9002
       X( N ) = X( N ) / A( 1 , N )
       JJ = 2
-      DO 160 I = 1 , NM
+      DO I = 1 , NM
          K = N-I
          TEMP = 0.D0
-         DO 170 J = 2 , JJ
+         DO J = 2 , JJ
             TEMP = A( J , K ) * X( K-1+J ) + TEMP
-  170    CONTINUE
+         ENDDO
          X( K ) = X( K ) - TEMP
          IF( JJ .LT. L ) JJ = JJ + 1
-  160 CONTINUE
+      ENDDO
 C
       IERR = 0
       RETURN
