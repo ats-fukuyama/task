5531c1c9f12af9475548a324e75928e0fde62199
Atsushi Fukuyama <fukuyama@nucleng.kyoto-u.ac.jp>
2007-05-01 06:40:02 +0000

    snap

--- lib/femtest.f
+++ lib/femtest.f
@@ -8,16 +8,29 @@
       complex(8),dimension(:),allocatable:: fvb,fvx
       real(8),dimension(:),allocatable:: rho,f1,f2
 
-      integer id,nrmax,mw,ml,nr,ierr
+      integer:: id,nrmax,npow,mw,ml,nr,ierr,nth,nph
+      real(8):: rf
 
       call gsopen
-!      id=1
-      id=2
+
+      id=3
       nrmax=11
+      npow=1
+      nth=0
+      nph=0
+      rf=1.d0
 
-      call initialize(nrmax)
+    1 write(6,'(A,3I5)') '## input: id,nrmax,npow=',id,nrmax,npow,nth,nph,rf
+      read(5,*,err=1,end=9000) id,nrmax,npow,nth,nph,rf
 
-      call fem_calc(id,nrmax)
+      select case(id)
+         case(1)
+            call fem_calc1(nrmax,npow)
+         case(2)
+            call fem_calc2(nrmax,npow)
+         case(3)
+            call fem_calc3(nrmax,npow,nth,nph,rf)
+      end select
 
       do ml=1,mlmax
          fvx(ml)=fvb(ml)
@@ -39,17 +52,20 @@
       call pages
       call femgr1d(0,rho,f2,nrmax,'@f1(rho)@')
       call pagee
+      goto 1
 
-      call gsclos
+ 9000 call gsclos
+      stop
 
       contains
 
 !----- set profile -----
 
-      subroutine initialize(nrmax)
+      subroutine mesh_init(nrmax,npow)
 
       implicit none
       integer,intent(in):: nrmax  ! number of points including end points
+      integer,intent(in):: npow   ! power of mesh position
       integer,save:: nrmax_save=0
       real(8):: drho
       integer:: nr
@@ -64,12 +80,12 @@
       endif
       nrmax_save=nrmax
 
-      drho=1.d0/(nrmax-1)
+      drho=1.d0/(nrmax-1)**npow
       do nr=1,nrmax
-         rho(nr)=drho*(nr-1)
+         rho(nr)=drho*(nr-1)**npow
       enddo
       return
-      end subroutine initialize
+      end subroutine mesh_init
 
 !----- allocate matrix and vectors -----
 
@@ -106,24 +122,19 @@
 
 !----- calculate coefficint matrix fma and source vector fvb -----
 
-      subroutine fem_calc(id,nrmax)
+      subroutine fem_calc1(nrmax,npow)
 
       use libfem_mod
       implicit none
-      integer,intent(in):: id     ! type of model 
-                                  !   1 : poisson eq (x)
-                                  !   2 : poisson eq (r)
       integer,intent(in):: nrmax  ! number of points including end points
+      integer,intent(in):: npow   ! power of mesh points
       integer:: nr,ml,mw,mc,nvmax,i,j,k
       real(8):: drho,val1,val2,val3,val4,temp
       real(8),dimension(4):: coef
 
-      select case(id)
-      case(1)
-         nvmax=2
-      case(2)
-         nvmax=2
-      end select
+      call mesh_init(nrmax,npow)
+
+      nvmax=2
 
       call fem_init(nrmax,nvmax)
 
@@ -135,10 +146,6 @@
       enddo
       mc=(mwmax+1)/2
       write(6,'(A,3I5)') 'mlmax,mwmax,mc=',mlmax,mwmax,mc
-
-      if(id.eq.1) then
-         nvmax=2
-         call fem_init(nrmax,nvmax)
 
          do nr=1,nrmax-1
             drho=rho(nr+1)-rho(nr)
@@ -174,9 +181,36 @@
          fma(mc,mlmax-1) = 1.d0
 
          fvb(1)=1.d0
-      else if(id.eq.2) then
-         nvmax=2
-         call fem_init(nrmax,nvmax)
+
+      return
+      end subroutine fem_calc1
+
+!----- calculate coefficint matrix fma and source vector fvb -----
+
+      subroutine fem_calc2(nrmax,npow)
+
+      use libfem_mod
+      implicit none
+      integer,intent(in):: nrmax  ! number of points including end points
+      integer,intent(in):: npow   ! power of mesh points
+      integer:: nr,ml,mw,mc,nvmax,i,j,k
+      real(8):: drho,val1,val2,val3,val4,temp
+      real(8),dimension(4):: coef
+
+      call mesh_init(nrmax,npow)
+
+      nvmax=2
+
+      call fem_init(nrmax,nvmax)
+
+      do ml=1,mlmax
+         fvb(ml)=0.d0
+         do mw=1,mwmax
+            fma(mw,ml)=0.d0
+         enddo
+      enddo
+      mc=(mwmax+1)/2
+      write(6,'(A,3I5)') 'mlmax,mwmax,mc=',mlmax,mwmax,mc
 
          do nr=1,nrmax-1
             drho=rho(nr+1)-rho(nr)
@@ -257,143 +291,271 @@
 
          fvb(1)=1.d0
          fvb(mlmax-1)=1.d0/11.d0
-      endif
-      return
-      end subroutine fem_calc
 
-c
-c=======================================================================
-      subroutine femgr1d(ngp,x,y,nxmax,str)
-c
-      implicit none
-      integer ngp,nxmax,nx
-      real*8, dimension(nxmax) :: x,y
-      real*4, dimension(:), allocatable :: gx,gy
-      character str*(*)
-      real*4 guclip
-c
-      allocate(gx(nxmax))
-      allocate(gy(nxmax))
-      do nx=1,nxmax
-         gx(nx)=guclip(x(nx))
-         gy(nx)=guclip(y(nx))
-      end do
-c
-      call grf1d(ngp,gx,gy,nxmax,nxmax,1,str,0)
-c
-      deallocate(gy)
-      deallocate(gx)
       return
-      end subroutine femgr1d
-c
-c=======================================================================
-      subroutine femgr2d(ngp,x,y,z,nxm,nxmax,nymax,str,ntype)
-c
+      end subroutine fem_calc2
+
+!----- calculate coefficint matrix fma and source vector fvb -----
+
+      subroutine fem_calc3(nrmax,npow,nth,nph,rf)
+
+      use libfem_mod
       implicit none
-      integer ngp,nxm,nxmax,nymax,nx,ny,ntype
-      real*8, dimension(nxmax) :: x
-      real*8, dimension(nymax) :: y
-      real*8, dimension(nxm,nymax) :: z
-      real*4, dimension(:), allocatable :: gx,gy
-      real*4, dimension(:,:), allocatable :: gz
-      integer, dimension(:,:,:), allocatable :: ka
-      character str*(*)
-      real*4 gxmin,gxmax,gsxmin,gsxmax,gxstep,gxorg
-      real*4 gymin,gymax,gsymin,gsymax,gystep,gyorg
-      real*4 gzmin,gzmax,gszmin,gszmax,gzstep,gzorg
-      real*4 gpx,gpy,gpratio,gpxc,gpyc
-      real*4 glx,gly,glratio
-      real*4 gp(4),gp3d(6)
-      real*4 guclip
-c
-      allocate(gx(nxmax))
-      allocate(gy(nymax))
-      allocate(gz(nxmax,nymax))
-      allocate(ka(8,nxmax,nymax))
-      do nx=1,nxmax
-         gx(nx)=guclip(x(nx))
-      end do
-      do ny=1,nymax
-         gy(ny)=guclip(y(ny))
-      end do
-      do ny=1,nymax
-         do nx=1,nxmax
-            gz(nx,ny)=guclip(z(nx,ny))
-         end do
-      end do
-C
-      call grfut1(gx,nxmax,gxmin,gxmax)
-      call grfut1(gy,nymax,gymin,gymax)
-      call grfut2(gz,nxm,nxmax,nymax,gzmin,gzmax)
-      if(gzmin*gzmax.lt.0.0) then
-         gzmax=max(abs(gzmin),abs(gzmax))
-         gzmin=-gzmax
-      endif
-c
-      call grfut3(gxmin,gxmax,gsxmin,gsxmax,gxstep,gxorg)
-      call grfut3(gymin,gymax,gsymin,gsymax,gystep,gyorg)
-      call grfut3(gzmin,gzmax,gszmin,gszmax,gzstep,gzorg)
-      gzstep=0.2*gzstep
-c
-      call grfut4(ngp,gp)
-c
-      if(ntype.eq.1.or.ntype.eq.2) then
-         glx=gsxmax-gsxmin
-         gly=gsymax-gsymin
-         gpx=gp(2)-gp(1)
-         gpy=gp(4)-gp(3)
-c
-         gpratio=gpy/gpx
-         glratio=gly/glx
-         if(glratio.gt.gpratio) then
-            gpxc=0.5*(gp(1)+gp(2))
-            gpx=gpy/glratio
-            gp(1)=gpxc-0.5*gpx
-            gp(2)=gpxc+0.5*gpx
-         elseif(glratio.lt.gpratio) then
-            gpyc=0.5*(gp(3)+gp(4))
-            gpy=gpx*glratio
-            gp(3)=gpyc-0.5*gpy
-            gp(4)=gpyc+0.5*gpy
-         endif
-      endif
-c
-      if(ntype.eq.1) then
-         call grf2dcx(gp,
-     &                gsxmin,gsxmax,gxstep,gxorg,
-     &                gsymin,gsymax,gystep,gyorg,
-     &                gszmin,gszmax,gzstep,gzorg,
-     &                gz,nxm,nxmax,nymax,str,ka,0)
-c
-      elseif(ntype.eq.2) then
-         call grf2dax(gp,
-     &                gsxmin,gsxmax,gxstep,gxorg,
-     &                gsymin,gsymax,gystep,gyorg,
-     &                gszmin,gszmax,gzstep,gzorg,
-     &                gz,nxm,nxmax,nymax,str,0,0)
-c
-      elseif(ntype.eq.3) then
-         gp3d(1)=10.0*1.5
-         gp3d(2)=20.0*1.5
-         gp3d(3)=10.0*1.5
-C         gp3d(4)=-60.0
-         gp3d(4)=-90.0
-C         gp3d(5)=65.0
-         gp3d(5)=30.0
-         gp3d(6)=100.0
-         call grf2dbx(gp,
-     &                gsxmin,gsxmax,gxstep,gxorg,
-     &                gsymin,gsymax,gystep,gyorg,
-     &                gszmin,gszmax,gzstep,gzorg,
-     &                gz,nxm,nxmax,nymax,
-     &                str,gp3d)
-      endif
-c
-      deallocate(ka)
-      deallocate(gz)
-      deallocate(gy)
-      deallocate(gx)
+      integer,intent(in):: nrmax  ! number of points including end points
+      integer,intent(in):: npow   ! power of mesh points
+      integer,intent(in):: nth    ! poloidal mode number
+      integer,intent(in):: nph    ! toroidal mode number
+      real(8),intent(in):: rf     ! wave frequency
+      integer:: nr,ml,mw,mc,nvmax,i,j,k,inod
+      real(8):: drho,rkth,rkph
+      complex(8),dimension(3,3,4,4):: fmc,fmd
+      complex(8),parameter:: ci=(0.d0,1.d0)
+
+      call mesh_init(nrmax,npow)
+
+      nvmax=6
+     
+      call fem_init(nrmax,nvmax)
+
+      do ml=1,mlmax
+         fvb(ml)=0.d0
+         do mw=1,mwmax
+            fma(mw,ml)=0.d0
+         enddo
+      enddo
+      mc=(mwmax+1)/2
+
+!      write(6,'(A,3I5)') 'mlmax,mwmax,mc=',mlmax,mwmax,mc
+
+      rkth=nth
+      rkph=nph
+      
+      do inod=1,4
+         do k=1,4
+            do j=1,3
+               do i=1,3
+                  fmc(i,j,k,inod)=0.d0
+               enddo
+            enddo
+         enddo
+      enddo
+
+      do inod=1,4
+         fmc(1,1,1,inod)= 1.d0-rkth**2+rkph**2
+         fmc(2,2,1,inod)= 1.d0-rkph**2
+         fmc(2,3,1,inod)=      rkth*rkph
+         fmc(3,2,1,inod)=      rkth*rkph
+         fmc(3,3,1,inod)= 1.d0-rkth**2
+      
+         fmc(2,1,2,inod)= ci*rkth
+         fmc(3,1,2,inod)= ci*rkph
+
+         fmc(1,2,3,inod)=-ci*rkth
+         fmc(1,3,3,inod)=-ci*rkph
+
+         fmc(2,2,4,inod)= -1.d0
+         fmc(3,3,4,inod)= -1.d0
+      enddo
+
+      do nr=1,nrmax-1
+         drho=rho(nr+1)-rho(nr)
+         ml=6*(nr-1)+1
+
+         do j=1,3
+            do i=1,3
+               do k=1,4
+                  fmd(i,j,k,1)=fmc(i,j,k,1)
+                  fmd(i,j,k,2)=0.5d0*(-11*fmc(i,j,k,1)+18*fmc(i,j,k,2)
+     &                                - 9*fmc(i,j,k,3)+ 2*fmc(i,j,k,4))
+                  fmd(i,j,k,3)=fmc(i,j,k,4)
+                  fmd(i,j,k,4)=0.5d0*(- 2*fmc(i,j,k,1)+ 9*fmc(i,j,k,2)
+     &                                -18*fmc(i,j,k,3)+11*fmc(i,j,k,4))
+               enddo
+            enddo
+         enddo
+
+         do j=1,3
+            do i=1,3
+               mw=mc+2*(j-1)-2*(i-1)
+
+! ----- non derivative terms
+
+               fma(mw  ,ml  )=fma(mw  ,ml  )
+     &                       +fmd(i,j,k,1)*table_hhh(k,1,1)*drho
+               fma(mw+1,ml  )=fma(mw+1,ml  )
+     &                       +fmd(i,j,k,1)*table_hhh(k,2,1)*drho*drho
+               fma(mw+6,ml  )=fma(mw+6,ml  )
+     &                       +fmd(i,j,k,1)*table_hhh(k,3,1)*drho
+               fma(mw+7,ml  )=fma(mw+7,ml  )
+     &                       +fmd(i,j,k,1)*table_hhh(k,4,1)*drho*drho
+
+               fma(mw-1,ml+1)=fma(mw-1,ml+1)
+     &                       +fmd(i,j,k,1)*table_hhh(k,1,2)*drho
+               fma(mw  ,ml+1)=fma(mw  ,ml+1)
+     &                       +fmd(i,j,k,1)*table_hhh(k,2,2)*drho*drho
+               fma(mw+5,ml+1)=fma(mw+5,ml+1)
+     &                       +fmd(i,j,k,1)*table_hhh(k,3,2)*drho
+               fma(mw+6,ml+1)=fma(mw+6,ml+1)
+     &                       +fmd(i,j,k,1)*table_hhh(k,4,2)*drho*drho
+
+               fma(mw-6,ml+6)=fma(mw-6,ml+6 )
+     &                       +fmd(i,j,k,1)*table_hhh(k,1,3)*drho
+               fma(mw-5,ml+6)=fma(mw-5,ml+6)
+     &                       +fmd(i,j,k,1)*table_hhh(k,2,3)*drho*drho
+               fma(mw  ,ml+6)=fma(mw  ,ml+6)
+     &                       +fmd(i,j,k,1)*table_hhh(k,3,3)*drho
+               fma(mw+1,ml+6)=fma(mw+1,ml+6)
+     &                       +fmd(i,j,k,1)*table_hhh(k,4,3)*drho*drho
+
+               fma(mw-7,ml+7)=fma(mw-7,ml+7)
+     &                       +fmd(i,j,k,1)*table_hhh(k,1,4)*drho
+               fma(mw-6,ml+7)=fma(mw-6,ml+7)
+     &                       +fmd(i,j,k,1)*table_hhh(k,2,4)*drho*drho
+               fma(mw-1,ml+7)=fma(mw-1,ml+7)
+     &                       +fmd(i,j,k,1)*table_hhh(k,3,4)*drho
+               fma(mw  ,ml+7)=fma(mw  ,ml+7)
+     &                       +fmd(i,j,k,1)*table_hhh(k,4,4)*drho*drho
+
+! ----- dw/dx terms
+
+               fma(mw  ,ml  )=fma(mw  ,ml  )
+     &                       +fmd(i,j,k,2)*table_hhh(k,5,1)*drho
+               fma(mw+1,ml  )=fma(mw+1,ml  )
+     &                       +fmd(i,j,k,2)*table_hhh(k,6,1)*drho*drho
+               fma(mw+6,ml  )=fma(mw+6,ml  )
+     &                       +fmd(i,j,k,2)*table_hhh(k,7,1)*drho
+               fma(mw+7,ml  )=fma(mw+7,ml  )
+     &                       +fmd(i,j,k,2)*table_hhh(k,8,1)*drho*drho
+
+               fma(mw-1,ml+1)=fma(mw-1,ml+1)
+     &                       +fmd(i,j,k,2)*table_hhh(k,5,2)*drho
+               fma(mw  ,ml+1)=fma(mw  ,ml+1)
+     &                       +fmd(i,j,k,2)*table_hhh(k,6,2)*drho*drho
+               fma(mw+5,ml+1)=fma(mw+5,ml+1)
+     &                       +fmd(i,j,k,2)*table_hhh(k,7,2)*drho
+               fma(mw+6,ml+1)=fma(mw+6,ml+1)
+     &                       +fmd(i,j,k,2)*table_hhh(k,8,2)*drho*drho
+
+               fma(mw-6,ml+6)=fma(mw-6,ml+6 )
+     &                       +fmd(i,j,k,2)*table_hhh(k,5,3)*drho
+               fma(mw-5,ml+6)=fma(mw-5,ml+6)
+     &                       +fmd(i,j,k,2)*table_hhh(k,6,3)*drho*drho
+               fma(mw  ,ml+6)=fma(mw  ,ml+6)
+     &                       +fmd(i,j,k,2)*table_hhh(k,7,3)*drho
+               fma(mw+1,ml+6)=fma(mw+1,ml+6)
+     &                       +fmd(i,j,k,2)*table_hhh(k,8,3)*drho*drho
+
+               fma(mw-7,ml+7)=fma(mw-7,ml+7)
+     &                       +fmd(i,j,k,2)*table_hhh(k,5,4)*drho
+               fma(mw-6,ml+7)=fma(mw-6,ml+7)
+     &                       +fmd(i,j,k,2)*table_hhh(k,6,4)*drho*drho
+               fma(mw-1,ml+7)=fma(mw-1,ml+7)
+     &                       +fmd(i,j,k,2)*table_hhh(k,7,4)*drho
+               fma(mw  ,ml+7)=fma(mw  ,ml+7)
+     &                       +fmd(i,j,k,2)*table_hhh(k,8,4)*drho*drho
+
+! ----- dE/dx terms
+
+               fma(mw  ,ml  )=fma(mw  ,ml  )
+     &                       +fmd(i,j,k,3)*table_hhh(k,1,5)*drho
+               fma(mw+1,ml  )=fma(mw+1,ml  )
+     &                       +fmd(i,j,k,3)*table_hhh(k,2,5)*drho*drho
+               fma(mw+6,ml  )=fma(mw+6,ml  )
+     &                       +fmd(i,j,k,3)*table_hhh(k,3,5)*drho
+               fma(mw+7,ml  )=fma(mw+7,ml  )
+     &                       +fmd(i,j,k,3)*table_hhh(k,4,5)*drho*drho
+
+               fma(mw-1,ml+1)=fma(mw-1,ml+1)
+     &                       +fmd(i,j,k,3)*table_hhh(k,1,6)*drho
+               fma(mw  ,ml+1)=fma(mw  ,ml+1)
+     &                       +fmd(i,j,k,3)*table_hhh(k,2,6)*drho*drho
+               fma(mw+5,ml+1)=fma(mw+5,ml+1)
+     &                       +fmd(i,j,k,3)*table_hhh(k,3,6)*drho
+               fma(mw+6,ml+1)=fma(mw+6,ml+1)
+     &                       +fmd(i,j,k,3)*table_hhh(k,4,6)*drho*drho
+
+               fma(mw-6,ml+6)=fma(mw-6,ml+6 )
+     &                       +fmd(i,j,k,3)*table_hhh(k,1,7)*drho
+               fma(mw-5,ml+6)=fma(mw-5,ml+6)
+     &                       +fmd(i,j,k,3)*table_hhh(k,2,7)*drho*drho
+               fma(mw  ,ml+6)=fma(mw  ,ml+6)
+     &                       +fmd(i,j,k,3)*table_hhh(k,3,7)*drho
+               fma(mw+1,ml+6)=fma(mw+1,ml+6)
+     &                       +fmd(i,j,k,3)*table_hhh(k,4,7)*drho*drho
+
+               fma(mw-7,ml+7)=fma(mw-7,ml+7)
+     &                       +fmd(i,j,k,3)*table_hhh(k,1,8)*drho
+               fma(mw-6,ml+7)=fma(mw-6,ml+7)
+     &                       +fmd(i,j,k,3)*table_hhh(k,2,8)*drho*drho
+               fma(mw-1,ml+7)=fma(mw-1,ml+7)
+     &                       +fmd(i,j,k,3)*table_hhh(k,3,8)*drho
+               fma(mw  ,ml+7)=fma(mw  ,ml+7)
+     &                       +fmd(i,j,k,3)*table_hhh(k,4,8)*drho*drho
+
+! ----- dw/dx dE/dx terms
+
+               fma(mw  ,ml  )=fma(mw  ,ml  )
+     &                       +fmd(i,j,k,4)*table_hhh(k,5,5)*drho
+               fma(mw+1,ml  )=fma(mw+1,ml  )
+     &                       +fmd(i,j,k,4)*table_hhh(k,6,5)*drho*drho
+               fma(mw+6,ml  )=fma(mw+6,ml  )
+     &                       +fmd(i,j,k,4)*table_hhh(k,7,5)*drho
+               fma(mw+7,ml  )=fma(mw+7,ml  )
+     &                       +fmd(i,j,k,4)*table_hhh(k,8,5)*drho*drho
+
+               fma(mw-1,ml+1)=fma(mw-1,ml+1)
+     &                       +fmd(i,j,k,4)*table_hhh(k,5,6)*drho
+               fma(mw  ,ml+1)=fma(mw  ,ml+1)
+     &                       +fmd(i,j,k,4)*table_hhh(k,6,6)*drho*drho
+               fma(mw+5,ml+1)=fma(mw+5,ml+1)
+     &                       +fmd(i,j,k,4)*table_hhh(k,7,6)*drho
+               fma(mw+6,ml+1)=fma(mw+6,ml+1)
+     &                       +fmd(i,j,k,4)*table_hhh(k,8,6)*drho*drho
+
+               fma(mw-6,ml+6)=fma(mw-6,ml+6 )
+     &                       +fmd(i,j,k,4)*table_hhh(k,5,7)*drho
+               fma(mw-5,ml+6)=fma(mw-5,ml+6)
+     &                       +fmd(i,j,k,4)*table_hhh(k,6,7)*drho*drho
+               fma(mw  ,ml+6)=fma(mw  ,ml+6)
+     &                       +fmd(i,j,k,4)*table_hhh(k,7,7)*drho
+               fma(mw+1,ml+6)=fma(mw+1,ml+6)
+     &                       +fmd(i,j,k,4)*table_hhh(k,8,7)*drho*drho
+
+               fma(mw-7,ml+7)=fma(mw-7,ml+7)
+     &                       +fmd(i,j,k,3)*table_hhh(k,5,8)*drho
+               fma(mw-6,ml+7)=fma(mw-6,ml+7)
+     &                       +fmd(i,j,k,3)*table_hhh(k,6,8)*drho*drho
+               fma(mw-1,ml+7)=fma(mw-1,ml+7)
+     &                       +fmd(i,j,k,3)*table_hhh(k,7,8)*drho
+               fma(mw  ,ml+7)=fma(mw  ,ml+7)
+     &                       +fmd(i,j,k,3)*table_hhh(k,8,8)*drho*drho
+            enddo
+         enddo
+      enddo
+
+      do mw=1,mwmax
+         fma(mw,1) = 0.d0
+         fma(mw,3) = 0.d0
+         fma(mw,5) = 0.d0
+      enddo
+      fma(mc,1)=1.d0
+      fma(mc,3)=1.d0
+      fma(mc,5)=1.d0
+
+      do mw=1,mwmax
+         fma(mw,mlmax-5) = 0.d0
+         fma(mw,mlmax-3) = 0.d0
+         fma(mw,mlmax-1) = 0.d0
+      enddo
+      fma(mc,mlmax-5) = 1.d0
+      fma(mc,mlmax-3) = 1.d0
+      fma(mc,mlmax-1) = 1.d0
+
+      fvb(mlmax-10)=1.d0
+
       return
-      end subroutine femgr2d
+      end subroutine fem_calc3
+
+      include 'femtest-sub.f'
 
       end program femtest
