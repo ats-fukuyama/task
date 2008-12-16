2aa3ec30229dc71df9c0e046a1542d011ca8ba4c
Atsushi Fukuyama <fukuyama@nucleng.kyoto-u.ac.jp>
2008-12-16 06:36:36 +0000

    snap

--- fp/fpcombine.f
+++ fp/fpcombine.f
@@ -3,20 +3,27 @@ C     $Id$
       INCLUDE 'fpcomm.inc'
       CHARACTER*80 KNAMFP0
 
+      MODEFR=0
+      MODEFW=0
+
     1 WRITE(6,*) '## How many input fpdatas?'
       READ(5,*,ERR=1,END=9000) NFMAX
 
-    2 WRITE(6,*) '## First fpdata file name?'
+    2 WRITE(6,*) '## First input fpdata file name?'
       READ(5,'(A)',ERR=2,END=9000) KNAMFP0
       CALL DPLDFP(KNAMFP0,NR0,RMIN,RMAX1)
 
       DO NFI=2,NFMAX
-    3    WRITE(6,'(A,I2,A)') '## #',NFI,' fpdata file name?'
+    3    WRITE(6,'(A,I2,A)') '## #',NFI,' input fpdata file name?'
          READ(5,'(A)',ERR=3,END=9000) KNAMFP0
          CALL DPLDFP(KNAMFP0,NR0,RMIN1,RMAX1)
       ENDDO
       RMAX=RMAX1
+      NRMAX=NR0
 
+      MODEFW=0
+    4 WRITE(6,*) '## Output fpdata file name?'
+      READ(5,'(A)',ERR=4,END=9000) KNAMFP
       CALL FPSAVE
  9000 CONTINUE
       STOP
@@ -27,6 +34,7 @@ C
       SUBROUTINE DPLDFP(KNAMFP0,NR0,RMIN1,RMAX1)
 C
       INCLUDE 'fpcomm.inc'
+      CHARACTER*80 KNAMFP0
 C      
       CALL FROPEN(21,KNAMFP0,0,MODEFR,'FP',IERR)
       IF(IERR.NE.0) THEN
@@ -46,8 +54,9 @@ C
       CLOSE(21)
 C
       WRITE(6,*) '# DATA WAS SUCCESSFULLY LOADED FROM THE FILE.'
-      write(6,'(A,2I5,1P2E12.4)') 'NR_MIN,NR_MAX,RMIN,RMAX=',
-     &     NR0+1,NR0+NRMAX,RMIN1,RMAX1
+      write(6,'(A,2I5,1P2E12.4,2I5)') 
+     &     'NRMIN/MAX,RMIN,RMAX,NPMAX,NTHMAX=',
+     &     NR0+1,NR0+NRMAX,RMIN1,RMAX1,NPMAX,NTHMAX
       RETURN
       END
 C
