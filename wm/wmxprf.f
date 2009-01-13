7122f475f962ef3a17e8b9f2d322c4fce3ee01f1
Mitsuru Honda <honda.mitsuru@jaea.go.jp>
2009-01-13 09:20:04 +0000

    wmxprf.f modified

--- wm/wmxprf.f
+++ wm/wmxprf.f
@@ -17,14 +17,17 @@ C         ZEFF   : effective charge number
 C         XRHO   : ro data of the calculated point ( sub. WMXRZF )
 C         PSIP   : psi data of the calculated point ( sub. EQGETP )
 C
-C     Output
-C       wmcomm.inc
-C         PN(I)  : density at center                   (1.0E20/m*3)
-C         PNS(I) : density on plasma surface           (1.0E20/m*3)
-C         PTPR(I): Parallel temperature at center      (keV)
-C         PTPP(I): Perpendicular temperature at center (keV)
+C         PNS(I) : density on plasma surface           (1.0E20/m3)
 C         PTS(I) : temperature on plasma surface       (keV)
 C         I=1:electron, I=2:Impurity, I=3:ion
+C
+C     Output
+C       wmcomm.inc
+C         PN(I)  : density at center                   (1.0E20/m3)
+C         PTPR(I): Parallel temperature at center      (keV)
+C         PTPP(I): Perpendicular temperature at center (keV)
+C         I=1:electron, I=2:Impurity, I=3:ion
+C
 C       wmxprf.inc
 C         NPRFI     : calculation mode for ion
 C         PN60(N,I) : density at the calculated point     (1/m3)
@@ -42,7 +45,7 @@ C
       REAL*8 UPRFNE(4,NXPRF), UPRFTE(4,NXPRF), UPRFTI(4,NXPRF)
       REAL*8 DERIV(NXPRF)
       REAL*8 ZEFFSV, PSIL, PPL, PTSSV(NSM), PNSSV(NSM)
-      REAL*8 XPAR1, XPAR2, XPAR
+      REAL*8 XPAR1, XPAR2, XPAR, PRFPSI_CTR
 C
       CHARACTER    TRFILE*80, CWK1*10
 C
@@ -91,24 +94,24 @@ C
 C----  Set ion calculation mode
 C
       IF (IP2.EQ.1) THEN
-        NPRFI = 0
-        WRITE(6,*)
-        WRITE(6,*)
-        WRITE(6,*)
-        DO WHILE ( NPRFI.NE.1.AND.NPRFI.NE.2 )
-          WRITE(6,*) 'SELECT MODE OF CALCULATION FOR ION'
-          WRITE(6,*) '  1 : USE PROFILE DATA FORM FILE'
-          WRITE(6,*) '  2 : USE DEFAULT METHOD OF WM CODE'
-          WRITE(6,*) 'SELECT 1 (DEFAULT) or 2 >>'
-          CWK1 = ' '
-          READ(5,'(A)',ERR=210) CWK1
-          IF (CWK1.EQ.' ') THEN
-            NPRFI = 1
-          ELSE
-            READ(CWK1,*,ERR=210) NPRFI
-          ENDIF
-       ENDDO
-  210  CONTINUE
+         NPRFI = 0
+         WRITE(6,*)
+         WRITE(6,*)
+         WRITE(6,*)
+         DO WHILE ( NPRFI.NE.1.AND.NPRFI.NE.2 )
+            WRITE(6,*) 'SELECT MODE OF CALCULATION FOR ION'
+            WRITE(6,*) '  1 : USE PROFILE DATA FORM FILE'
+            WRITE(6,*) '  2 : USE DEFAULT METHOD OF WM CODE'
+            WRITE(6,*) 'SELECT 1 (DEFAULT) or 2 >>'
+            CWK1 = ' '
+            READ(5,'(A)',ERR=210) CWK1
+            IF (CWK1.EQ.' ') THEN
+               NPRFI = 1
+            ELSE
+               READ(CWK1,*,ERR=210) NPRFI
+            ENDIF
+         ENDDO
+ 210     CONTINUE
       ENDIF
 C
 C----  Open profile data file and read
@@ -121,9 +124,16 @@ C
       OPEN ( IFNO, FILE=TRFILE, ERR=9998 )
       READ ( IFNO, '(I3)', END=9999, ERR=9999 ) NPRF
       DO N=1,NPRF
-        READ ( IFNO, '(4E14.7)', END=9999, ERR=9999 )
-     >                          PRFPSI(N), PRFNE(N), PRFTE(N), PRFTI(N)
+         READ ( IFNO, '(4E14.7)', END=9999, ERR=9999 )
+     >        PRFPSI(N), PRFNE(N), PRFTE(N), PRFTI(N)
       ENDDO
+C
+      ! ***** Added tentatively by M. Honda *****
+      PRFPSI_CTR = PRFPSI(1)
+      DO N=1,NPRF
+         PRFPSI(N) = 2.D0 * PI * (PRFPSI(N) - PRFPSI_CTR)
+      ENDDO
+      ! *****************************************
 C
 C----  Set coefficient for spline
 C
@@ -138,43 +148,43 @@ C----  Set profile data at the point calculated in wm-code.
 C----    PSIP  : psi value at the point ( calculated at subroutine eqpsic )
 C
       DO NR=1,NRMAX1
-        IF (XRHO(NR).GT.1.0D0) THEN
-          PN60(NR,1) = 0.0D0
-          PT60(NR,1) = PRFTE(NPRF)*PTS(1)
-          DO NS=2,NSMAX
-             PN60(NR,NS) = 0.0D0
-             PT60(NR,NS) = PRFTI(NPRF)*PTS(NS)
-          ENDDO
-        ELSE
-          PSIL=PSIP(NR)
-          CALL SPL1DF(PSIL,PPL,PRFPSI,UPRFNE,NPRF,IRC)
-          PN60(NR,1)=PPL*PNS(1)
-          CALL SPL1DF(PSIL,PPL,PRFPSI,UPRFTE,NPRF,IRC)
-          PT60(NR,1)=PPL*PTS(1)
-          CALL SPL1DF(PSIL,PPL,PRFPSI,UPRFTI,NPRF,IRC)
-          PT60(NR,2)=PPL*PTS(2)
-C----  not need PNS(2)
-          PN60(NR,2)=(1.D0-(PZ(2)-ZEFF)/(PZ(2)-XPAR))/PZ(2)*PN60(NR,1)
-          DO NS=3,NSMAX
-             PN60(NR,NS)=(PZ(2)-ZEFF)/(PZ(2)-XPAR)/XPAR2*PN60(NR,1)
-     &                  *PNS(NS)
-             PT60(NR,NS)=PPL*PTS(NS)
-          ENDDO
-        ENDIF
+         IF (XRHO(NR).GT.1.0D0) THEN
+            PN60(NR,1) = 0.0D0
+            PT60(NR,1) = PRFTE(NPRF)*PTS(1)
+            DO NS=2,NSMAX
+               PN60(NR,NS) = 0.0D0
+               PT60(NR,NS) = PRFTI(NPRF)*PTS(NS)
+            ENDDO
+         ELSE
+            PSIL=PSIP(NR)
+            CALL SPL1DF(PSIL,PPL,PRFPSI,UPRFNE,NPRF,IRC)
+            PN60(NR,1)=PPL*PNS(1)
+            CALL SPL1DF(PSIL,PPL,PRFPSI,UPRFTE,NPRF,IRC)
+            PT60(NR,1)=PPL*PTS(1)
+            CALL SPL1DF(PSIL,PPL,PRFPSI,UPRFTI,NPRF,IRC)
+            PT60(NR,2)=PPL*PTS(2)
+C---- not need PNS(2)
+            PN60(NR,2)=(1.D0-(PZ(2)-ZEFF)/(PZ(2)-XPAR))/PZ(2)*PN60(NR,1)
+            DO NS=3,NSMAX
+               PN60(NR,NS)=(PZ(2)-ZEFF)/(PZ(2)-XPAR)/XPAR2*PN60(NR,1)
+     &              *PNS(NS)
+               PT60(NR,NS)=PPL*PTS(NS)
+            ENDDO
+         ENDIF
       ENDDO
 C     
 C----  Change the value at center and surface
 C
-      PN(1)  = PRFNE(1)*1.0D-20*PNS(1)
-      PTPR(1) = PRFTE(1)*1.0D-3*PTS(1)
-      PTPP(1) = PRFTE(1)*1.0D-3*PTS(1)
+      PN(1)   = PRFNE(1)*1.0D-20*PNS(1)
+      PTPR(1) = PRFTE(1)*1.0D-3 *PTS(1)
+      PTPP(1) = PRFTE(1)*1.0D-3 *PTS(1)
 C
       IF (NPRFI.EQ.1) THEN
 C----  not need PNS(2)
          PN(2)=(1.D0-(PZ(2)-ZEFF)/(PZ(2)-XPAR))/PZ(2)*PN(1)
          PTPR(2) = PRFTI(1)*1.0D-3*PTS(2)
          PTPP(2) = PRFTI(1)*1.0D-3*PTS(2)
-C
+C     
          DO NS=3,NSMAX
             PN(NS)=(PZ(2)-ZEFF)/(PZ(2)-XPAR)/XPAR2*PN(1)*PNS(NS)
             PTPR(NS) = PRFTI(1)*1.D-3*PTS(NS)
@@ -184,14 +194,18 @@ C
 C
 C----  Debug write
 C
-C     WRITE(6,8000)
-C     DO 8010 N=1,NRMAX1
-C       WRITE(6,'(I3,1P7E10.3)') N, XRHO(N), PSIP(N), PRFPSI(N)
-C    >                                     , PRFNE(N), PNE(N)
-C    >                                     , PRFTI(N), PTI(N)
-C8010 CONTINUE
-C8000 FORMAT(' N ',3X,'XRHO',6X,'PSIP ',4X,'PRFPSI',5X
-C    >      ,'PRFNE',6X,'PNE',6X,'PRFTI',6X,'PTI')
+c$$$      WRITE(6,8000)
+c$$$      DO N=1,NPRF
+c$$$         WRITE(6,'(I3,1P4(1XE10.3))') N,PRFPSI(N),PRFNE(N),
+c$$$     >                                  PRFTE(N),PRFTI(N)
+c$$$      ENDDO
+c$$$      WRITE(6,8010)
+c$$$      DO N=1,NRMAX1
+c$$$         WRITE(6,'(I3,1P4(1XE10.3))') N, XRHO(N), PSIP(N),
+c$$$     >                                   PN60(N,1), PT60(N,2)
+c$$$      ENDDO
+c$$$ 8000 FORMAT(' N ',3X,'PRFPSI',6X,'PRFNE',6X,'PRFTE',6X,'PRFTI')
+c$$$ 8010 FORMAT(' N ',4X,'XRHO',7X,'PSIP ',6X,'PNE',8X,'PTI')
 C
       NRMAXSV=NRMAX
       NTHMAXSV=NTHMAX
@@ -210,6 +224,9 @@ C
       CLOSE( IFNO )
  9998 CONTINUE
       WRITE (6,*) '==========  WMXPRF  END   =========='
+      GO TO 9996
  9997 CONTINUE
-      RETURN
+      WRITE (6,*) '     *****  NO IMPURITY  *****      '
+      WRITE (6,*) '======  WMXPRF  ABNORMAL END  ======'
+ 9996 RETURN
       END
