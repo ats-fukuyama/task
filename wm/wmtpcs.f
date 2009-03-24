b2529982af72db62079ee071ab3b552c71325d6f
Mitsuru Honda <honda.mitsuru@jaea.go.jp>
2009-03-24 05:56:14 +0000

    bug fixed in multimode calc

--- wm/wmtpcs.f
+++ wm/wmtpcs.f
@@ -7,45 +7,67 @@ c
       SUBROUTINE WM_TOPICS(IERR)
 c
       include '../wm/wmcomm.inc'
-      DIMENSION PABSR_SV(NRM,NSM,MNPHMX)
+      DIMENSION PABSR_SV(NRM,NSM,MNPHMX), PABSTL(NSM), FACT(MNPHMX)
 c
       NPH0_SV  = NPH0
       PRFIN_SV = PRFIN
 C
-      WRITE(6,*) "========== MULTI-MODE CALCULATION =========="
+      WRITE(6,*) "========= MULTI-MODE CALCULATION START ========="
       DO NM = 1, MNPHMAX
          NPH0 = MNPH0(NM)
          PRFIN = PRFIN_SV * PFRAC(NM)
          WRITE(6,*)
-         WRITE(6,*) "== Toroidal mode number : ",MNPH0(NM)
-         WRITE(6,*) "== Power fraction       : ",PFRAC(NM)
+         WRITE(6,'(A,I4)') "== Toroidal mode number : ",MNPH0(NM)
+         WRITE(6,'(A,F8.6)') "== Power fraction       : ",PFRAC(NM)
 C
          CALL WMEXEC(IERR)
          CALL MPSYNC
          IF(IERR.NE.0) EXIT
 C
          DO NS = 1, NSMAX
+            PABSTL(NS) = 0.D0
             DO NR = 1, NRMAX
                PABSR_SV(NR,NS,NM) = PABSR(NR,NS)
+               PABSTL(NS) = PABSTL(NS) + PABSR(NR,NS)
             ENDDO
          ENDDO
+C
+         PABSTTL = 0.D0
+         DO NS = 1, NSMAX
+            PABSTTL = PABSTTL + PABSTL(NS)
+         ENDDO
+C
+         IF(PRFIN.GT.0.D0.AND.PABSTT.GT.0.D0) THEN
+            FACT(NM)=PRFIN/PABSTTL
+         ELSE
+            FACT(NM)=1.D0
+         ENDIF
       ENDDO
 C
       IF(IERR.EQ.0.AND.MYRANK.EQ.0) THEN
+         PABSTTM = 0.D0
          DO NS = 1, NSMAX
+            PABSTL(NS) = 0.D0
             DO NR = 1, NRMAX
                PABSRM = 0.D0
                DO NM = 1, MNPHMAX
-                  PABSRM = PABSRM + PABSR_SV(NR,NS,NM)
+                  PABSRM = PABSRM + FACT(NM) * PABSR_SV(NR,NS,NM)
                ENDDO
                PABSR(NR,NS) = PABSRM
+               PABSTL(NS) = PABSTL(NS) + PABSRM
             ENDDO
+            PABSTTM = PABSTTM + PABSTL(NS)
          ENDDO
          CALL WM_TOPICS_OUT
       ENDIF
 C
       NPH0  = NPH0_SV
       PRFIN = PRFIN_SV
+C
+      WRITE(6,*)
+      WRITE(6,'(A,1PE12.4)')  "TOT. PABS=",PABSTTM
+      WRITE(6,'(A,1P6E12.4)') "PABS(NS) =",(PABSTL(NS),NS=1,NSMAX)
+      WRITE(6,*)
 C
       WRITE(6,*) "======= MULTI-MODE CALCULATION NORMAL END ======="
       WRITE(6,*)
