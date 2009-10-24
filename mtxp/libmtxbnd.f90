e9bf70451f0d43c303b6ec8bb4863f03f9e204f9
Atsushi Fukuyama <fukuyama@nucleng.kyoto-u.ac.jp>
2009-10-24 01:12:47 +0000

    snap

--- mtxp/libmtxbnd.f90
+++ mtxp/libmtxbnd.f90
@@ -8,16 +8,9 @@
 
       MODULE libmtx
 
-      IMPLICIT NONE
+      use libmpi
+
       PUBLIC mtx_initialize
-      PUBLIC mtx_setup
-      PUBLIC mtx_set_matrix
-      PUBLIC mtx_set_source
-      PUBLIC mtx_set_vector
-      PUBLIC mtx_solve
-      PUBLIC mtx_get_vector
-      PUBLIC mtx_gather_vector
-      PUBLIC mtx_cleanup
       PUBLIC mtx_finalize
       PUBLIC mtx_barrier
       PUBLIC mtx_broadcast_character
@@ -29,8 +22,17 @@
       PUBLIC mtx_allgather_integer
       PUBLIC mtx_gatherv_real8
       PUBLIC mtx_allgatherv_real8
-      PRIVATE
 
+      PUBLIC mtx_setup
+      PUBLIC mtx_set_matrix
+      PUBLIC mtx_set_source
+      PUBLIC mtx_set_vector
+      PUBLIC mtx_solve
+      PUBLIC mtx_get_vector
+      PUBLIC mtx_gather_vector
+      PUBLIC mtx_cleanup
+
+      PRIVATE
       INTEGER:: imax,jmax,joffset,ierr
       REAL(8),DIMENSION(:),POINTER:: x,b
       REAL(8),DIMENSION(:,:),POINTER:: A
@@ -41,17 +43,8 @@
 
       CONTAINS
 
-      SUBROUTINE mtx_initialize(rank_,size_)
-      INTEGER,INTENT(OUT):: rank_,size_
-
-      WRITE(6,'(A)') '# mtx_initialize: libmtxbnd'
-      rank_=0
-      size_=1
-      RETURN
-      END SUBROUTINE mtx_initialize
-
       SUBROUTINE mtx_setup(imax_,istart_,iend_,jwidth_)
-
+      IMPLICIT NONE
       INTEGER,INTENT(IN):: imax_           ! total matrix size
       INTEGER,INTENT(OUT):: istart_,iend_  ! allocated range of lines 
       INTEGER,INTENT(IN):: jwidth_         ! band matrix width
@@ -77,6 +70,7 @@
       END SUBROUTINE mtx_setup
 
       SUBROUTINE mtx_set_matrix(i,j,v)
+      IMPLICIT NONE
       INTEGER,INTENT(IN):: i,j  ! matrix position i=line, j=row
       REAL(8),INTENT(IN):: v    ! value to be inserted
 
@@ -84,15 +78,8 @@
       RETURN
       END SUBROUTINE mtx_set_matrix
       
-      SUBROUTINE mtx_add_matrix(i,j,v)
-      INTEGER,INTENT(IN):: i,j  ! matrix position i=line, j=row
-      REAL(8),INTENT(IN):: v    ! value to be inserted
-
-      A(j-i+joffset,i)=A(j-i+joffset,i)+v
-      RETURN
-      END SUBROUTINE mtx_add_matrix
-      
       SUBROUTINE mtx_set_source(j,v)
+      IMPLICIT NONE
       INTEGER,INTENT(IN):: j ! vector positon j=row
       REAL(8),INTENT(IN):: v ! value to be inserted
 
@@ -100,15 +87,8 @@
       RETURN
       END SUBROUTINE mtx_set_source
       
-      SUBROUTINE mtx_add_source(j,v)
-      INTEGER,INTENT(IN):: j ! vector positon j=row
-      REAL(8),INTENT(IN):: v ! value to be inserted
-
-      b(j)=b(j)+v
-      RETURN
-      END SUBROUTINE mtx_add_source
-      
       SUBROUTINE mtx_set_vector(j,v)
+      IMPLICIT NONE
       INTEGER,INTENT(IN):: j ! vector positon j=row
       REAL(8),INTENT(IN):: v ! value to be inserted
 
@@ -117,6 +97,7 @@
       END SUBROUTINE mtx_set_vector
       
       SUBROUTINE mtx_add_vector(j,v)
+      IMPLICIT NONE
       INTEGER,INTENT(IN):: j ! vector positon j=row
       REAL(8),INTENT(IN):: v ! value to be inserted
 
@@ -125,10 +106,12 @@
       END SUBROUTINE mtx_add_vector
       
       SUBROUTINE mtx_split_operation
+      IMPLICIT NONE
       RETURN
       END SUBROUTINE mtx_split_operation
       
       SUBROUTINE mtx_solve(itype,tolerance,its)
+      IMPLICIT NONE
       INTEGER,INTENT(IN):: itype     ! not used
       REAL(8),INTENT(IN):: tolerance ! not used
       INTEGER,INTENT(OUT):: its
@@ -160,6 +143,7 @@
       END SUBROUTINE mtx_solve
 
       SUBROUTINE mtx_get_vector(j,v)
+      IMPLICIT NONE
       INTEGER,INTENT(IN):: j
       REAL(8),INTENT(OUT):: v
       v=x(j)
@@ -167,8 +151,10 @@
       END SUBROUTINE mtx_get_vector
 
       SUBROUTINE mtx_gather_vector(v)
+      IMPLICIT NONE
       REAL(8),DIMENSION(imax),INTENT(OUT):: v
       INTEGER:: i
+
       DO i=1,imax
          v(i)=x(i)
       ENDDO
@@ -176,99 +162,11 @@
       END SUBROUTINE mtx_gather_vector
 
       SUBROUTINE mtx_cleanup
+      IMPLICIT NONE
 
       DEALLOCATE(x,b)
       DEALLOCATE(A)
       RETURN
       END SUBROUTINE mtx_cleanup
-
-      SUBROUTINE mtx_finalize
-      RETURN
-      END SUBROUTINE mtx_finalize
-
-      SUBROUTINE mtx_barrier
-      RETURN
-      END SUBROUTINE mtx_barrier
-
-      SUBROUTINE mtx_broadcast_character(kdata,n)
-      CHARACTER(LEN=n),INTENT(INOUT):: kdata
-      INTEGER,INTENT(IN):: n      
-      RETURN
-      END SUBROUTINE mtx_broadcast_character
-
-      SUBROUTINE mtx_broadcast_integer(idata,n)
-      INTEGER,DIMENSION(n),INTENT(INOUT):: idata
-      INTEGER,INTENT(IN):: n      
-      RETURN
-      END SUBROUTINE mtx_broadcast_integer
-
-      SUBROUTINE mtx_broadcast_real8(vdata,n)
-      REAL(8),DIMENSION(n),INTENT(INOUT):: vdata
-      INTEGER,INTENT(IN):: n
-            RETURN
-      END SUBROUTINE mtx_broadcast_real8
-
-      SUBROUTINE mtx_broadcast_complex8(vdata,n)
-      COMPLEX(8),DIMENSION(n),INTENT(INOUT):: vdata
-      INTEGER,INTENT(IN):: n
-            RETURN
-      END SUBROUTINE mtx_broadcast_complex8
-
-      SUBROUTINE mtx_gather_integer(idata,itot)
-
-      INTEGER,INTENT(IN):: idata
-      INTEGER,DIMENSION(1),INTENT(OUT):: itot
-
-      itot(1)=idata
-      RETURN
-      END SUBROUTINE mtx_gather_integer
-
-      SUBROUTINE mtx_gather_real8(ddata,dtot)
-
-      REAL(8),INTENT(IN):: ddata
-      REAL(8),DIMENSION(1),INTENT(OUT):: dtot
-
-      dtot(1)=ddata
-      RETURN
-      END SUBROUTINE mtx_gather_real8
-
-      SUBROUTINE mtx_allgather_integer(idata,itot)
-
-      INTEGER,INTENT(IN):: idata
-      INTEGER,DIMENSION(1),INTENT(OUT):: itot
-
-      itot(1)=idata
-      RETURN
-      END SUBROUTINE mtx_allgather_integer
-
-      SUBROUTINE mtx_gatherv_real8(vdata,ndata,vtot,ntot,ilena,iposa)
-
-      INTEGER,INTENT(IN):: ndata
-      INTEGER,INTENT(INOUT):: ntot
-      REAL(8),DIMENSION(ndata),INTENT(IN):: vdata
-      REAL(8),DIMENSION(ntot),INTENT(OUT):: vtot
-      INTEGER,DIMENSION(1):: ilena,iposa
-      INTEGER:: n
-
-      DO n=1,ndata
-         vtot(n)=vdata(n)
-      ENDDO
-      RETURN
-      END SUBROUTINE mtx_gatherv_real8
-
-      SUBROUTINE mtx_allgatherv_real8(vdata,ndata,vtot,ntot,ilena,iposa)
-
-      INTEGER,INTENT(IN):: ndata
-      INTEGER,INTENT(INOUT):: ntot
-      REAL(8),DIMENSION(ndata),INTENT(IN):: vdata
-      REAL(8),DIMENSION(ntot),INTENT(OUT):: vtot
-      INTEGER,DIMENSION(1):: ilena,iposa
-      INTEGER:: n
-
-      DO n=1,ndata
-         vtot(n)=vdata(n)
-      ENDDO
-      RETURN
-      END SUBROUTINE mtx_allgatherv_real8
 
       END MODULE libmtx
---
+++ mtxp/libmtxnompi.f90
@@ -0,0 +1,114 @@
+!     $Id$
+
+      MODULE libmpi
+
+      integer:: rank,size
+
+      CONTAINS
+
+      SUBROUTINE mtx_initialize(rank_,size_)
+      IMPLICIT NONE
+      INTEGER,INTENT(OUT):: rank_,size_
+
+      WRITE(6,'(A)') '# mtx_initialize: libmtxbnd'
+      rank=0
+      size=1
+      rank_=0
+      size_=1
+      RETURN
+      END SUBROUTINE mtx_initialize
+
+      SUBROUTINE mtx_finalize
+      RETURN
+      END SUBROUTINE mtx_finalize
+
+      SUBROUTINE mtx_barrier
+      RETURN
+      END SUBROUTINE mtx_barrier
+
+      SUBROUTINE mtx_broadcast_character(kdata,n)
+      IMPLICIT NONE
+      CHARACTER(LEN=n),INTENT(INOUT):: kdata
+      INTEGER,INTENT(IN):: n      
+      RETURN
+      END SUBROUTINE mtx_broadcast_character
+
+      SUBROUTINE mtx_broadcast_integer(idata,n)
+      IMPLICIT NONE
+      INTEGER,DIMENSION(n),INTENT(INOUT):: idata
+      INTEGER,INTENT(IN):: n      
+      RETURN
+      END SUBROUTINE mtx_broadcast_integer
+
+      SUBROUTINE mtx_broadcast_real8(vdata,n)
+      IMPLICIT NONE
+      REAL(8),DIMENSION(n),INTENT(INOUT):: vdata
+      INTEGER,INTENT(IN):: n
+            RETURN
+      END SUBROUTINE mtx_broadcast_real8
+
+      SUBROUTINE mtx_broadcast_complex8(vdata,n)
+      IMPLICIT NONE
+      COMPLEX(8),DIMENSION(n),INTENT(INOUT):: vdata
+      INTEGER,INTENT(IN):: n
+            RETURN
+      END SUBROUTINE mtx_broadcast_complex8
+
+      SUBROUTINE mtx_gather_integer(idata,itot)
+      IMPLICIT NONE
+      INTEGER,INTENT(IN):: idata
+      INTEGER,DIMENSION(1),INTENT(OUT):: itot
+
+      itot(1)=idata
+      RETURN
+      END SUBROUTINE mtx_gather_integer
+
+      SUBROUTINE mtx_gather_real8(ddata,dtot)
+      IMPLICIT NONE
+      REAL(8),INTENT(IN):: ddata
+      REAL(8),DIMENSION(1),INTENT(OUT):: dtot
+
+      dtot(1)=ddata
+      RETURN
+      END SUBROUTINE mtx_gather_real8
+
+      SUBROUTINE mtx_allgather_integer(idata,itot)
+      IMPLICIT NONE
+      INTEGER,INTENT(IN):: idata
+      INTEGER,DIMENSION(1),INTENT(OUT):: itot
+
+      itot(1)=idata
+      RETURN
+      END SUBROUTINE mtx_allgather_integer
+
+      SUBROUTINE mtx_gatherv_real8(vdata,ndata,vtot,ntot,ilena,iposa)
+      IMPLICIT NONE
+      INTEGER,INTENT(IN):: ndata
+      INTEGER,INTENT(INOUT):: ntot
+      REAL(8),DIMENSION(ndata),INTENT(IN):: vdata
+      REAL(8),DIMENSION(ntot),INTENT(OUT):: vtot
+      INTEGER,DIMENSION(1):: ilena,iposa
+      INTEGER:: n
+
+      DO n=1,ndata
+         vtot(n)=vdata(n)
+      ENDDO
+      RETURN
+      END SUBROUTINE mtx_gatherv_real8
+
+      SUBROUTINE mtx_allgatherv_real8(vdata,ndata,vtot,ntot,ilena,iposa)
+      IMPLICIT NONE
+      INTEGER,INTENT(IN):: ndata
+      INTEGER,INTENT(INOUT):: ntot
+      REAL(8),DIMENSION(ndata),INTENT(IN):: vdata
+      REAL(8),DIMENSION(ntot),INTENT(OUT):: vtot
+      INTEGER,DIMENSION(1):: ilena,iposa
+      INTEGER:: n
+
+      DO n=1,ndata
+         vtot(n)=vdata(n)
+      ENDDO
+      RETURN
+      END SUBROUTINE mtx_allgatherv_real8
+
+      END MODULE libmpi
