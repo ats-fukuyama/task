2463a09253b157e8d35dd8c6169bde64d4dc44ef
Atsushi Fukuyama <fukuyama@nucleng.kyoto-u.ac.jp>
2013-01-14 16:47:49 +0000

    correction

---
+++ mtxp/libmtxdmumps.f90
@@ -0,0 +1,363 @@
+!     $Id$
+
+! -----------------------------------------------------------------------
+!
+!  Description: Solves a linear system with DMUMPS (direct method)
+!
+! -----------------------------------------------------------------------
+
+      MODULE libmtx
+
+      USE mpi
+      USE libmpi
+      PRIVATE
+
+      PUBLIC mtx_initialize
+      PUBLIC mtx_finalize
+      PUBLIC mtx_set_communicator
+      PUBLIC mtx_reset_communicator
+
+      PUBLIC mtx_setup
+      PUBLIC mtx_set_matrix
+      PUBLIC mtx_set_source
+      PUBLIC mtx_set_vector
+      PUBLIC mtx_solve
+      PUBLIC mtx_get_vector
+      PUBLIC mtx_gather_vector
+      PUBLIC mtx_cleanup
+
+      TYPE(mtx_mpi_type):: mtx_global
+      INTEGER:: ncomm,nrank,nsize
+
+      INCLUDE 'dmumps_struc.h'
+      TYPE (DMUMPS_STRUC) id
+      INTEGER,DIMENSION(:),POINTER:: istartx,iendx,isizex,nz_tot
+      REAL(8),DIMENSION(:),POINTER:: b,b_loc
+      INTEGER:: imax,istart,iend,jwidth,nzcount,nzmax_save
+
+! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+!                 Beginning of program
+! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+      CONTAINS
+
+      SUBROUTINE mtx_initialize(nrank_,nsize_)
+      IMPLICIT NONE
+      INTEGER,INTENT(OUT):: nrank_,nsize_
+      INTEGER:: ierr
+
+
+      CALL MPI_Init(ierr)
+      IF(ierr.NE.0) WRITE(6,*) &
+           'XX mtx_initialize: MPI_Init: ierr=',ierr
+      ncomm=MPI_COMM_WORLD
+      CALL mtx_set_communicator_global(ncomm,nrank,nsize)
+      nsize_=nsize
+      nrank_=nrank
+      mtx_global%comm=ncomm
+      mtx_global%rank=nrank
+      mtx_global%size=nsize
+      mtx_global%rankg=0
+      mtx_global%sizeg=1
+      mtx_global%rankl=nrank
+      mtx_global%sizel=nsize
+      return
+      END SUBROUTINE mtx_initialize
+
+!-----
+
+      SUBROUTINE mtx_finalize
+      IMPLICIT NONE
+      INTEGER:: ierr
+
+      CALL MPI_Finalize(ierr)
+      IF(ierr.NE.0) WRITE(6,*) &
+           'XX mtx_finalize: MPI_Finalize: ierr=',ierr
+      END SUBROUTINE mtx_finalize
+
+!-----
+
+      SUBROUTINE mtx_set_communicator(mtx_mpi,nrank_,nsize_)
+        IMPLICIT NONE
+        TYPE(mtx_mpi_type),INTENT(IN):: mtx_mpi
+        INTEGER,INTENT(OUT):: nrank_,nsize_
+
+        CALL mtx_set_communicator_local(mtx_mpi)
+        ncomm=mtx_mpi%comm
+        nrank=mtx_mpi%rank
+        nsize=mtx_mpi%size
+        nrank_=nrank
+        nsize_=nsize
+        return
+      END SUBROUTINE mtx_set_communicator
+
+!-----
+
+      SUBROUTINE mtx_reset_communicator(nrank_,nsize_)
+        IMPLICIT NONE
+        INTEGER,INTENT(OUT):: nrank_,nsize_
+
+        CALL mtx_reset_communicator_local
+        ncomm=mtx_global%comm
+        nrank=mtx_global%rank
+        nsize=mtx_global%size
+        nrank_=nrank
+        nsize_=nsize
+        return
+      END SUBROUTINE mtx_reset_communicator
+
+      SUBROUTINE mtx_setup(imax_,istart_,iend_,jwidth,nzmax)
+
+      INTEGER,INTENT(IN):: imax_           ! total matrix size
+      INTEGER,INTENT(OUT):: istart_,iend_  ! allocated range of lines 
+      INTEGER,OPTIONAL,INTENT(IN):: jwidth ! band matrix width
+      INTEGER,OPTIONAL,INTENT(IN):: nzmax  ! number of nonzero components
+      INTEGER:: i,jmax,iwork1,iwork2
+
+!     ----- define a communicator -----      
+      id%COMM=ncomm
+!     ----- unsymmetric matrix -----      
+      id%SYM=0
+!     ----- host working -----      
+      id%PAR=1
+!     ----- initialize MUMPS -----
+      id%JOB=-1
+
+      CALL DMUMPS(id)
+
+      imax=imax_
+      IF(PRESENT(jwidth)) THEN
+         jmax=jwidth
+      ELSE
+         jmax=2*imax-1
+      ENDIF
+
+      iwork1 = imax/nsize
+      iwork2 = mod(imax,nsize)
+      istart =  nrank   *iwork1 + min(nrank,  iwork2) + 1
+      iend   = (nrank+1)*iwork1 + min(nrank+1,iwork2)
+      
+      istart_=istart
+      iend_=iend
+      IF(PRESENT(nzmax)) THEN
+         nzmax_save=nzmax
+      ELSE
+         nzmax_save=(iend-istart+1)*jmax
+      ENDIF
+
+!      if(nrank.eq.0) then
+!         write(6,'(A,3I10)') 'nrank,nsize,nzmax=',nrank,nsize,nzmax_l
+!         write(6,'(A,3I10)') 'imax,istart,iend=',imax,istart,iend
+!      endif
+!      if(nrank.eq.1) then
+!         write(21,'(A,3I10)') 'nrank,nsize,nzmax=',nrank,nsize,nzmax_l
+!         write(21,'(A,3I10)') 'imax,istart,iend=',imax,istart,iend
+!      endif
+!      if(nrank.eq.2) then
+!         write(22,'(A,3I10)') 'nrank,nsize,nzmax=',nrank,nsize,nzmax_l
+!         write(22,'(A,3I10)') 'imax,istart,iend=',imax,istart,iend
+!      endif
+!      if(nrank.eq.3) then
+!         write(23,'(A,3I10)') 'nrank,nsize,nzmax=',nrank,nsize,nzmax_l
+!         write(23,'(A,3I10)') 'imax,istart,iend=',imax,istart,iend
+!      endif
+
+      ALLOCATE(id%IRN_loc(nzmax_save))
+      ALLOCATE(id%JCN_loc(nzmax_save))
+      ALLOCATE(id%A_loc(nzmax_save))
+      ALLOCATE(id%RHS(imax))
+      nzcount=0
+      DO i=1,imax
+         id%RHS(i)=0.d0
+      ENDDO
+      ALLOCATE(istartx(0:nsize-1),iendx(0:nsize-1),isizex(0:nsize-1))
+      ALLOCATE(nz_tot(0:nsize-1))
+      ALLOCATE(b(imax),b_loc(iend-istart+1))
+      RETURN
+      END SUBROUTINE mtx_setup
+      
+      SUBROUTINE mtx_set_matrix(i,j,v)
+      INTEGER,INTENT(IN):: i,j  ! matrix position i=line, j=row
+      REAL(8),INTENT(IN):: v    ! value to be inserted
+
+      IF(i.GE.istart.AND.i.LE.iend) THEN
+         nzcount=nzcount+1
+         IF(nzcount > nzmax_save) THEN
+            WRITE(6,*) "XX mtx_set_matrix: nzcount > nzmax_save:", &
+                       nzcount,nzmax_save
+            WRITE(6,*) " at component (i,j): ",i,j
+            STOP
+         END IF
+         id%A_loc(nzcount)=v
+         id%IRN_loc(nzcount)=i
+         id%JCN_loc(nzcount)=j
+      ELSE
+         write(6,'(A)') &
+              'XX libmtxdmumps:mtx_set_matrix: i : out of range'
+         write(6,'(A,4I10)') '   nrank,istart,iend,i=',nrank,istart,iend,i
+      ENDIF
+      return
+      END SUBROUTINE mtx_set_matrix
+      
+      SUBROUTINE mtx_set_source(j,v)
+      INTEGER,INTENT(IN):: j ! vector positon j=row
+      REAL(8),INTENT(IN):: v ! value to be inserted
+
+      IF(j.GE.istart.AND.j.LE.iend) THEN
+         b_loc(j-istart+1)=v
+      ELSE
+         write(6,'(A)') &
+              'XX libmtxdmumps:mtx_set_source: j : out of range'
+         write(6,'(A,4I10)') '   nrank,istart,iend,j=',nrank,istart,iend,j
+      ENDIF
+      RETURN
+      END SUBROUTINE mtx_set_source
+      
+      SUBROUTINE mtx_set_vector(j,v)
+      INTEGER,INTENT(IN):: j ! vector positon j=row
+      REAL(8),INTENT(IN):: v ! value to be inserted
+
+      return
+      END SUBROUTINE mtx_set_vector
+      
+      SUBROUTINE mtx_solve(itype,tolerance,its, &
+           methodKSP,methodPC,damping_factor,emax,emin,max_steps)
+      INTEGER,INTENT(IN):: itype     ! info level
+      REAL(8),INTENT(IN):: tolerance
+      INTEGER,INTENT(OUT):: its
+      INTEGER,OPTIONAL:: methodKSP,methodPC,max_steps
+      REAL(8),OPTIONAL:: damping_factor,emax,emin
+      INTEGER:: i,isum
+
+      call mtx_allgather1_integer(istart-1,istartx)
+      call mtx_allgather1_integer(iend,iendx)
+      do i=0,nsize-1
+         isizex(i)=iendx(i)-istartx(i)
+      enddo
+
+      call mtx_allgather1_integer(nzcount,nz_tot)
+      isum=0
+      do i=0,nsize-1
+         isum=isum+nz_tot(i)
+      enddo
+
+!      if(nrank.eq.0) then
+!         write(21,'(A,3I10)') 'imax,isum,nzcount=',imax,isum,nzcount
+!         do i=0,nsize-1
+!            write(21,'(A,4I10)') 'nrank,istartx,iendx,isizex=', &
+!                  i,istartx(i),iendx(i),isizex(i)
+!         enddo
+!      endif
+!      if(nrank.eq.1) then
+!         write(22,'(A,3I10)') 'imax,isum,nzcount=',imax,isum,nzcount
+!         do i=0,nsize-1
+!            write(22,'(A,4I10)') 'nrank,istartx,iendx,isizex=', &
+!                  i,istartx(i),iendx(i),isizex(i)
+!         enddo
+!      endif
+
+      call mtx_allgatherv_real8(b_loc,iend-istart+1,b,imax,isizex,istartx)
+      do i=1,imax
+         id%RHS(i)=b(i)
+      enddo
+
+      id%N=imax
+      id%NZ=isum
+      id%NZ_loc=nzcount
+      id%NRHS=1
+      id%LRHS=imax
+!     ----- distributed matrix -----
+      id%ICNTL(18)=3
+!     ----- error output level contrall -----
+      IF(itype.EQ.0) THEN
+         id%ICNTL(1)=0
+         id%ICNTL(2)=0
+         id%ICNTL(3)=0
+         id%ICNTL(4)=0
+      ELSE IF(itype.EQ.1) THEN
+         id%ICNTL(1)=6
+         id%ICNTL(2)=0
+         id%ICNTL(3)=0
+         id%ICNTL(4)=1
+      ELSE IF(itype.GE.2.AND.itype.LE.4) THEN
+         id%ICNTL(1)=6
+         id%ICNTL(2)=0
+         id%ICNTL(3)=6
+         id%ICNTL(4)=itype
+      ELSE
+         id%ICNTL(1)=6
+         id%ICNTL(2)=0
+         id%ICNTL(3)=6
+         id%ICNTL(4)=2
+      END IF
+
+!      if(nrank.eq.0) then
+!         do i=1,nzcount
+!            write(21,'(A,2I10,1PE12.4)') 'i,j,A=', &
+!                 id%IRN_loc(i),id%JCN_loc(i),id%A_loc(i)
+!         enddo
+!         do i=1,imax
+!            write(21,'(A,I10,1PE12.4)') 'i,b=',i,id%RHS(i)
+!         enddo
+!         do i=istart,iend
+!            write(21,'(A,I10,1PE12.4)') 'i,b_loc=',i,b_loc(i)
+!         enddo
+!      endif
+!      if(nrank.eq.1) then
+!         do i=1,nzcount
+!            write(22,'(A,2I10,1PE12.4)') 'i,j,A=', &
+!                 id%IRN_loc(i),id%JCN_loc(i),id%A_loc(i)
+!         enddo
+!         do i=1,imax
+!            write(22,'(A,I10,1PE12.4)') 'i,b=',i,b(i)
+!         enddo
+!         do i=istart,iend
+!            write(22,'(A,I10,1PE12.4)') 'i,b_loc=',i,b_loc(i)
+!         enddo
+!      endif
+
+      id%JOB=6
+      CALL dmumps(id)
+
+      its=0
+      RETURN
+      END SUBROUTINE mtx_solve
+
+      SUBROUTINE mtx_get_vector(j,v)
+
+      INTEGER,INTENT(IN):: j
+      REAL(8),INTENT(OUT):: v
+
+      v=id%RHS(j)
+      RETURN
+      END SUBROUTINE mtx_get_vector
+
+      SUBROUTINE mtx_gather_vector(v)
+
+      REAL(8),DIMENSION(imax),INTENT(OUT):: v
+      INTEGER:: j
+
+      DO j=1,imax
+         v(j)=id%RHS(j)
+      ENDDO
+      RETURN
+      END SUBROUTINE mtx_gather_vector
+!!!!!!
+      SUBROUTINE mtx_cleanup
+
+      id%JOB = -2
+      CALL DMUMPS(id)
+
+      DEALLOCATE(id%IRN_loc)
+      DEALLOCATE(id%JCN_loc)
+      DEALLOCATE(id%A_loc)
+      DEALLOCATE(id%RHS)
+
+      DEALLOCATE(istartx,iendx,isizex)
+      DEALLOCATE(nz_tot)
+      DEALLOCATE(b,b_loc)
+
+      RETURN
+      END SUBROUTINE mtx_cleanup
+
+      END MODULE libmtx
---
+++ mtxp/testmtxc.f90
@@ -0,0 +1,141 @@
+!     Test program of libmtxc for 1D Shroedinger equation
+!     coded by Y.maruyama
+
+PROGRAM testmtxc
+
+  USE libmpi
+  USE libmtxc
+  USE libgrf,only:grd1d
+
+  IMPLICIT NONE
+  integer :: nrank,nprocs
+  integer :: isize,itype,idata(2)
+  integer :: istart,iend,its
+  integer :: imax,jwidth,jsource
+  integer :: ndiv,i,j
+  integer :: MODE
+  real(8) :: tolerance,ddata(1)
+  real(8) :: xmin,xmax,dt,dx
+  real(4) :: cputime1,cputime2
+  complex(8),dimension(:),pointer:: x
+  complex(8) :: k
+  character :: character*1
+  real(8),dimension(:)  ,pointer :: FX
+  real(8),dimension(:,:),pointer :: FY
+  character ::STR*80
+
+  call mtx_initialize(nrank,nprocs)
+  if(nrank.eq.0) then
+     write(6,*) 'nrank, nprocs = ',nrank,nprocs
+     call gsopen
+  endif
+
+  itype = 0
+  tolerance=1.d-7
+ 
+  dt   = 0.05
+  xmin = 0.d0
+  xmax = 1.d0
+  ndiv = 101
+
+  if(nrank.eq.0) then
+     write(6,'(A)') "#TEST PROGRAM : Schroedinger equation solver"
+1    continue
+     write(6,"(A,/E12.4,1X,2I4,D12.4)")&
+          &  "#INPUT: dt,ndiv,itype=",&
+          &           dt,ndiv,itype
+     read(5,*,ERR=1)  dt,ndiv,itype
+
+     isize = ndiv
+
+     idata(1)= isize
+     idata(2)= itype
+     ddata(1)= tolerance
+
+  end if
+
+  call mtx_broadcast_integer(idata,2)
+  call mtx_broadcast_real8(ddata,1)
+
+  isize     = idata(1)
+  itype     = idata(2)
+  tolerance = ddata(1)
+
+  dx = (xmax-xmin)/DBLE(ndiv-1)
+  k = dt/((0.d0, 1.d0)*(dx**2))
+
+  if(nrank.eq.0) call cpu_time(cputime1)
+
+  imax = isize
+  jwidth = 3
+  allocate(x(imax))
+
+  do i=1,imax
+     x(i)=exp(-20.0D0*((i-1)*dx-0.5D0)**2)
+  end do
+
+100 continue
+
+  call mtx_setup(imax,istart,iend,jwidth=jwidth)
+
+  do i=istart,iend
+     if(i.gt.1) call mtx_set_matrix(i,i-1,k)
+     call mtx_set_matrix(i,i,1-2*k)
+     if(i.lt.isize) call mtx_set_matrix(i,i+1,k)
+  end do
+
+  do i=istart,iend
+     call mtx_set_source(i,x(i))
+  end do
+
+  call mtx_solve(itype,tolerance,its)
+  if(nrank.eq.0) then
+     write(6,*) 'Iteration Number=',its
+  end if
+
+  call mtx_gather_vector(x)
+
+2 continue
+
+  if(nrank.eq.0) then
+
+     allocate(FX(isize),FY(isize,2))
+     do i=1,isize
+        FX(i) = dx*(i-1)
+     end do
+        do i=1,isize
+           FY(i,1) = real(x(i))
+           FY(i,2) = aimag(x(i))
+           write(6,'(i5,1p3e12.4)') i,FX(i),FY(i,1),FY(i,2)
+        end do
+     STR="@test1@"
+     MODE = 0
+     call pages
+     call GRD1D(0,FX,FY,isize,isize,2,STR,MODE)
+     call pagee
+     deallocate(FX,FY)
+
+3    continue
+     write(6,*) "#INPUT: (C)CONTINUE,(Q)QUIT"
+     read (5,*,ERR=3) character
+  endif
+  CALL mtx_broadcast_character(character,1)
+
+  call mtx_cleanup
+  if (character.eq."c")then
+     goto 100
+  else if(character.ne."q")then
+     go to 2
+  end if
+  
+  deallocate(x)
+  if(nrank.eq.0) then
+     call cpu_time(cputime2)
+     write(6,'(A,F12.3)') &
+          '--cpu time =',cputime2-cputime1
+     call gsclos
+  end if
+  call mtx_finalize
+
+  stop
+end program testmtxc
