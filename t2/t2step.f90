7ac59625c5dbff738a56fbf2207063cbcccfef32
Haruki Seto <seto.haruki@jaea.go.jp>
2014-01-30 12:42:39 +0000

    *** empty log message ***

--- t2/t2step.f90
+++ t2/t2step.f90
@@ -23,7 +23,7 @@ CONTAINS
          i0pmax, i0cmax, i0vmax, i0xmax, i0bmax, i0nlct,&
          d0eps,&
          i1nlct, d1rsdl,&
-         d1gsm,  d1grv,  d1guv,  d1guv_after, d1guv_befor, &
+         d2xvec, d2xvec_befor, d2xvec_after,&
          nconvmax,eps_conv
     
     USE T2CALV, ONLY: T2_CALV
@@ -33,15 +33,17 @@ CONTAINS
     INTEGER(i0ikind),INTENT(OUT):: i_conv
     REAL(i0rkind),INTENT(OUT):: residual_conv
     INTEGER(i0ikind):: nconv
-    INTEGER(i0ikind):: i0pflg, i1
-    REAL(   i0rkind):: d0aft,  d0bfr, d0dif, d0ave, d0dif_max
+    INTEGER(i0ikind):: i0xidi,i0vidi
     CHARACTER(10)::c10nl
     REAL(4):: e0time_0,e0time_1
     
     c10nl='NL'
-
-    DO i1=1,i0xmax
-       d1guv_befor(i1)= d1guv(i1)
+    
+    DO i0xidi = 1, i0xmax
+       DO i0vidi = 1, i0vmax
+          d2xvec_befor( i0vidi,i0xidi)&
+               = d2xvec(i0vidi,i0xidi)
+       ENDDO
     ENDDO
     
     DO nconv=1,nconvmax
@@ -66,7 +68,7 @@ CONTAINS
        !C CONVERGENCE CHECK
        !C
        
-       CALL T2_STEP_CONV(residual_conv)
+       CALL T2STEP_CONV(residual_conv)
        
        i_conv=nconv
        IF(residual_conv.LE.eps_conv) EXIT
@@ -75,87 +77,83 @@ CONTAINS
        !C UPDATRE VARIABLES FOR NEXT ITERATION 
        !C
        
-       DO i1=1,i0xmax
-          d1guv_befor(i1) = d1guv_after(i1)
+       DO i0xidi = 1, i0xmax
+          DO i0vidi = 1, i0vmax
+             d2xvec_befor(       i0vidi,i0xidi)&
+                  = d2xvec_after(i0vidi,i0xidi)
+          ENDDO
        ENDDO
        
     ENDDO
     
-    DO i1=1,i0xmax
-       d1guv(i1) = d1guv_after(i1)
+    DO i0xidi = 1, i0xmax
+       DO i0vidi = 1, i0vmax
+          d2xvec(i0vidi,i0xidi) = d2xvec_after(i0vidi,i0xidi)
+       ENDDO
     ENDDO
     
     RETURN
     
   END SUBROUTINE T2_STEP
   
-  SUBROUTINE T2_STEP_CONV(d0dif)
+  SUBROUTINE T2STEP_CONV(d0dif)
     
     USE T2COMM, ONLY:&
-         i0spcs,i0dbg,i0vmax,i0nmax2,i0nmax4,&
-         i1mfc4,d0eps,d1guv_after,d1guv_befor
+         i0vmax,i0xmax,&
+         i1mfc4,d2xvec_after,d2xvec_befor
     
     REAL(   i0rkind),INTENT(OUT)::d0dif
-    INTEGER(i0ikind):: i0xnt, i1, i2
+    INTEGER(i0ikind):: i0xidi, i0vidi
     REAL(   i0rkind):: d0aft, d0bfr, d0ave, d0dif_tmp,d0dif_max
+    REAL(   i0rkind):: d1dif(1:i0vmax),d1ave(1:i0vmax)
     
     d0dif_max = 0.D0    
+    d1dif(1:i0vmax) = 0.D0
+    d1ave(1:i0vmax) = 0.D0
 
-    DO i1=1,i0vmax
-       !IF((i1.GE.6).AND.(MOD(i1-6,8).GE.i0dbg)) CYCLE
-       IF(i1.NE.3) CYCLE
-!       IF(    i1.GT.3)THEN
-
-          d0dif     = 0.D0
-          d0ave     = 0.D0
-          
-          DO i2=0,i0nmax2-1
-             
-             i0xnt = i0vmax*i2 + i1
-             d0aft = d1guv_after(i0xnt)
-             d0bfr = d1guv_befor(i0xnt)
-             d0dif = d0dif + (d0aft-d0bfr)**2
-             d0ave = d0ave + d0aft**2
-             
-          ENDDO
-          
- !      ELSEIF(i1.LE.3)THEN
- !         
- !         d0dif     = 0.D0
- !         d0ave     = 0.D0
- !         
- !         DO i2=1,i0nmax4
- !            i0xnt = i1mfc4(i2) - 1
- !            i0xnt = i0vmax*i0xnt + i1
- !            d0aft = d1guv_after(i0xnt)
- !            d0bfr = d1guv_befor(i0xnt)
- !            d0dif = d0dif + (d0aft-d0bfr)**2
- !            d0ave = d0ave + d0aft**2
- !         ENDDO
- !         
- !      ENDIF
-      
-       d0dif  = SQRT(d0dif)
-       d0ave  = SQRT(d0ave)
+    DO i0xidi = 1, i0xmax  
+       DO i0vidi = 4,i0vmax
+          d0aft = d2xvec_after(i0vidi,i0xidi)
+          d0bfr = d2xvec_befor(i0vidi,i0xidi)
+          d1dif(i0vidi) = d1dif(i0vidi) + (d0aft-d0bfr)**2
+          d1ave(i0vidi) = d1ave(i0vidi) + d0aft**2
+       ENDDO
+    ENDDO
+     
+    !
+    !
+    ! FOR 1D VARIABLES
+    !
+    !
+    
+    !C
+    !C
+    !C
+    
+    DO i0vidi = 1,i0vmax
+       
+       d0dif  = d1dif(i0vidi)
+       d0ave  = d1ave(i0vidi)
        
        IF(d0ave.LE.0.D0)THEN
           WRITE(6,'("*********************************************")')
           WRITE(6,'("       ERROR IN T2_STEP_CONVERGENCE          ")')
           WRITE(6,'("       INDETERMINATE PROBLEM                 ")')
           WRITE(6,'("*********************************************")')
-          PRINT*,i1
-          RETURN
+          PRINT*,i0vidi
+          STOP
        ENDIF
        
        d0dif_tmp = d0dif/d0ave
+       d0dif_tmp = SQRT(d0dif_tmp)
+       WRITE(6,*),'VARIABLES=',i0vidi,'RESIDUAL=',d0dif_tmp
        d0dif_max = MAX(d0dif_max,d0dif_tmp)
-       WRITE(6,*),'VARIABLES=',i1,'RESIDUAL=',d0dif_tmp
     ENDDO
     
     d0dif = d0dif_max
     
     RETURN
     
-  END SUBROUTINE T2_STEP_CONV
+  END SUBROUTINE T2STEP_CONV
   
 END MODULE T2STEP
