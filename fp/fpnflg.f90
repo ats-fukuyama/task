aa144309308d8cd2950bb525ac3796197005d163
Kazuki Nagai <nagai@p-grp.nucleng.kyoto-u.ac.jp>
2016-02-04 19:03:33 +0900

    FPNFLG parallelization

--- fp/fpnflg.f90
+++ fp/fpnflg.f90
@@ -40,45 +40,52 @@
       USE libmpi
       IMPLICIT NONE
       INTEGER,INTENT(IN):: NR,ID
-      INTEGER:: L, K, L2, NSB1, NSB2, NSA1, NSA2, NP1, NP2, NTH
-      REAL(8):: RSUM, FACT, RSUM2, FACT1, FACT2, FACT3, FACT4
-      REAL(8),DIMENSION(NPMAX,0:LLMAX_NF):: R1, R2       
-      REAL(8),dimension(0:LLMAX_NF,NPMAX,NSAMAX):: F_LG1, F_LG2
+      INTEGER:: L, K, L2, NSB1, NSB2, NSA1, NSA2, NP1, NP2, NTH, VLOC
+      REAL(8):: RSUM, FACT, RSUM2, FACT1, FACT2, FACT3, FACT4, FACT5
+      REAL(8):: RSUM_SUM, double_count
+      REAL(8),DIMENSION(0:LLMAX_NF,NPSTART:NPEND):: F_LG1, F_LG2, R1, R2
+      REAL(8),DIMENSION(0:LLMAX_NF,NPMAX):: F_LG1_ALL, F_LG2_ALL, R1_SUM, R2_SUM
 
       NSB1=NSB1_NF(ID)
       NSB2=NSB2_NF(ID)
       NSA1=NSA1_NF(ID)
       NSA2=NSA2_NF(ID)
 
+      double_count=1.D0
+      IF(ID.eq.1.or.ID.eq.2.or.ID.eq.5) double_count=0.5D0
+
+      FACT = RNFD0(NSB1)*RNFD0(NSB2)*1.D20*double_count
+
 !      Decomposing the distribution function into Legendre harmonics
 
-      DO NP1=1,NPMAX
+      DO NP1=NPSTART,NPEND
          DO L=0,LLMAX_NF
-            F_LG1(L,NP1,NSB1)=0.D0
-            F_LG2(L,NP1,NSB2)=0.D0
+            F_LG1(L,NP1)=0.D0
+            F_LG2(L,NP1)=0.D0
             DO NTH=1,NTHMAX
-               F_LG1(L,NP1,NSB1)=F_LG1(L,NP1,NSB1) &
+               F_LG1(L,NP1)=F_LG1(L,NP1) &
                     +FNSB(NTH,NP1,NR,NSB1) &
                     *PL_NF(L,NTH)*SINM(NTH)*DELTH
-               F_LG2(L,NP1,NSB2)=F_LG2(L,NP1,NSB2) &
+               F_LG2(L,NP1)=F_LG2(L,NP1) &
                     +FNSB(NTH,NP1,NR,NSB2) &
                     *PL_NF(L,NTH)*SINM(NTH)*DELTH
             END DO
-            F_LG1(L,NP1,NSB1)=0.5D0*(2.D0*L+1.D0)*F_LG1(L,NP1,NSB1)
-            F_LG2(L,NP1,NSB2)=0.5D0*(2.D0*L+1.D0)*F_LG2(L,NP1,NSB2)
+            F_LG1(L,NP1)=0.5D0*(2.D0*L+1.D0)*F_LG1(L,NP1)
+            F_LG2(L,NP1)=0.5D0*(2.D0*L+1.D0)*F_LG2(L,NP1)
          END DO
       END DO
+      CALL mtx_set_communicator(comm_np)
+      CALL mtx_allgather_real8(F_LG1,(LLMAX_NF+1)*(NPEND-NPSTART+1),F_LG1_ALL)
+      CALL mtx_allgather_real8(F_LG2,(LLMAX_NF+1)*(NPEND-NPSTART+1),F_LG2_ALL)
 
-      FACT = RNFD0(NSB1)*RNFD0(NSB2)*1.D20
-
-      DO NP1=1,NPMAX
+      DO NP1=NPSTART,NPEND
          DO NTH=1,NTHMAX
             RATE_NF_D1(NTH,NP1,NR,ID)=0.D0
             RATE_NF_D2(NTH,NP1,NR,ID)=0.D0
          END DO
          DO L=0,LLMAX_NF
-            R1(NP1,L)=0.D0
-            R2(NP1,L)=0.D0
+            R1(L,NP1)=0.D0
+            R2(L,NP1)=0.D0
          END DO
       END DO
 
@@ -90,28 +97,32 @@
          FACT1=2.D0/(2.D0*L+1)
          DO K=0,LLMAX_NF
             FACT2=2.D0/(2.D0*K+1)
-            DO NP1=1,NPMAX
+            DO NP1=NPSTART,NPEND
                FACT3=2.D0*PI*DELP(NSB1)*PM(NP1,NSB1)**2
                DO NP2=1,NPMAX
                   FACT4=2.D0*PI*DELP(NSB2)*PM(NP2,NSB2)**2
+                  FACT5=2.D0*PI*DELP(NSB1)*PM(NP2,NSB1)**2
                   RSUM=RSUM+FACT*FACT1*FACT2*FACT3*FACT4 &
-                       *F_LG1(L,NP1,NSB1)*F_LG2(K,NP2,NSB2) &
+                       *F_LG1_ALL(L,NP1)*F_LG2_ALL(K,NP2) &
                        *SIGMAV_LG(L,K,NP1,NP2,ID)
 
-                  R1(NP1,L)=R1(NP1,L)+FACT*FACT2*FACT4 &
+                  R1(L,NP1)=R1(L,NP1)+FACT*FACT2*FACT4 &
                        *SIGMAV_LG(L,K,NP1,NP2,ID) &
-                       *F_LG2(K,NP2,NSB2)
+                       *F_LG2_ALL(K,NP2)
 
-                  R2(NP2,K)=R2(NP2,K)+FACT*FACT1*FACT3 &
-                       *SIGMAV_LG(L,K,NP1,NP2,ID) &
-                       *F_LG1(L,NP1,NSB1)
+                  R2(K,NP1)=R2(K,NP1)+FACT*FACT1*FACT5 &
+                       *SIGMAV_LG(L,K,NP2,NP1,ID) &
+                       *F_LG1_ALL(L,NP2)
                END DO
             END DO
          END DO
       END DO
 
+      CALL mtx_allreduce1_real8(RSUM,3,RSUM_SUM,VLOC)
+      RSUM=RSUM_SUM
+
       DO NTH=1,NTHMAX
-         DO NP1=1,NPMAX
+         DO NP1=NPSTART,NPEND
             DO L=0,LLMAX_NF
                RATE_NF_D1(NTH,NP1,NR,ID)= &
                     RATE_NF_D1(NTH,NP1,NR,ID) &
@@ -123,14 +134,17 @@
          END DO
       END DO
 
-      RATE_NF(NR,ID)=RSUM
-      IF((N_IMPL.eq.0.or.N_IMPL.gt.LMAXFP).and.NR.eq.1)THEN
-         WRITE(6,*) '|-NF_REACTION_RATE:', nrank, comm_nr%rank
-         WRITE(6,'(A,3I5,A,2I5)') '   |-ID,NSB1,NSB2 -> NSA1,NSA2=' &
-              ,ID,NSB1,NSB2,' -> ',NSA1,NSA2
-         WRITE(6, *) "  |-ID,  NR,NSB1,NSB2,  <sigma*v>,      ENG1_NF"
-         WRITE(6,'("  ",4I5,1PE14.6,1PE12.4)') ID,NR,NSB1,NSB2,RATE_NF(NR,ID)/FACT,ENG1_NF(ID)
+      CALL mtx_reset_communicator
+
+      IF((N_IMPL.eq.0.or.N_IMPL.gt.LMAXFP).and.NR.eq.1.and.NPSTART.eq.1)THEN
+         WRITE(6,'(A,3I4)') '|-NF_REACTION_RATE:', nrank, comm_nr%rank, comm_nsa%rank
+         WRITE(6,'(A,I5,A,2I5,A,2I5)') '   |-ID,NSB1,NSB2 -> NSA1,NSA2=' &
+              ,ID,':  ',NSB1,NSB2,' -> ',NSA1,NSA2
+         WRITE(6, *) "  |-ID,  NR,NSB1,NSB2,  <sigma*v>,      ENG1_NF,      RATE_NF"
+         WRITE(6,'("  ",4I5,1PE14.6,1PE12.4, 1P2E14.6)') ID,NR,NSB1,NSB2,RSUM/FACT,ENG1_NF(ID), RSUM
       END IF
+
+      RATE_NF(NR,ID)=RSUM
 
       WRITE(6,*) 'RATE_NF', RATE_NF(NR,ID), 'ID', ID
 
