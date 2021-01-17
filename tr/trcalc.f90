ac88f8be2bca3b4d180dd8c666d22c83332ca97b
Atsushi Fukuyama <fukuyama@afportal.net>
2021-01-17 17:33:16 +0900

    tr: correction ambiguous

--- tr/trcalc.f90
+++ tr/trcalc.f90
@@ -478,7 +478,9 @@
          F34TEFF=FT/(1.D0+(1.D0-0.1D0*FT)*SQRT(RNUE)   +0.5D0*(1.D0-0.5D0*FT)*RNUE/ZEFFL)
 
          SALFA0=-1.17D0*(1.D0-FT)/(1.D0-0.22D0*FT-0.19D0*FT**2)
-         WRITE(6,'(A,I6,3ES12.4)') 'SALFA:',NR,SALFA0,FT,RNUI
+
+!         WRITE(6,*) '@@@ point 3'
+!         WRITE(6,'(A,I6,3ES12.4)') 'SALFA:',NR,SALFA0,FT,RNUI
          SALFA=((SALFA0+0.25D0*(1.D0-FT**2)*SQRT(RNUI)) /(1.D0+0.5D0*SQRT(RNUI))+0.315D0*RNUI**2*FT**6) &
      &        /(1.D0+0.15D0*RNUI**2*FT**6)
 
