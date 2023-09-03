168ce9862f22092ab8057ffd80cc0ba877ab459d
Atsushi Fukuyama <fukuyama@afportal.net>
2023-09-03 14:24:07 +0900

    wrx/wrxexecr.f90 updated

--- wrx/wrexecr.f90
+++ wrx/wrexecr.f90
@@ -167,7 +167,7 @@ CONTAINS
     YN(6,nstp)= RK_Z
     YN(7,nstp)= UU
     YN(8,nstp)= 0.D0
-    CALL wr_write_line(NSTP,YN(0,NSTP),YN(1:7,NSTP),YN(8,NSTP))
+    CALL wr_write_line(NSTP,YN(0,NSTP),YN(1:NEQ,NSTP),YN(8,NSTP))
     
     ! --- set magnetic field and minor radius at the start point ---
 
@@ -224,7 +224,7 @@ CONTAINS
           YN(6,nstp)= RK_Z
           YN(7,nstp)= UU
           YN(8,nstp)= 0.D0
-          CALL wr_write_line(NSTP,YN(0,NSTP),YN(1:7,NSTP),YN(8,NSTP))
+          CALL wr_write_line(NSTP,YN(0,NSTP),YN(1:NEQ,NSTP),YN(8,NSTP))
 
           
           ! --- If the ray is out of region, exit mode=11 ---
@@ -451,6 +451,7 @@ CONTAINS
     RKY =YN(5,nstp)
     RKZ =YN(6,nstp)
     UU  =YN(7,nstp)
+    INITIAL_DRV=0
 
     IF(idebug_wr(8).NE.0) THEN
        rk=SQRT(rkx**2+rky**2+rkz**2)
@@ -1035,6 +1036,129 @@ CONTAINS
     REAL(rkind),INTENT(IN):: X,Y(NEQ)
     REAL(rkind),INTENT(OUT):: F(7)
     REAL(rkind):: VV,TT,RF,XP,YP,ZP,RKXP,RKYP,RKZP,UU,OMG,ROMG
+    REAL(rkind):: DROMG,DRXP,DRYP,DRZP,DRKXP,DRKYP,DRKZP
+    REAL(rkind):: DOMG,DXP,DYP,DZP,DKXP,DKYP,DKZP,DS
+    REAL(rkind):: VX,VY,VZ,VKX,VKY,VKZ,VDU
+    REAL(rkind):: DUMMY,delk
+    REAL(rkind),SAVE:: DXP_save,DYP_save,DZP_save
+    REAL(rkind),SAVE:: DKXP_save,DKYP_save,DKZP_save
+
+    VV=DELDER
+    TT=DELDER
+    DUMMY=X
+
+    RF=wr_nray_status%RF
+    XP=Y(1)
+    YP=Y(2)
+    ZP=Y(3)
+    RKXP=Y(4)
+    RKYP=Y(5)
+    RKZP=Y(6)
+    UU=Y(7)
+
+    OMG=2.D6*PI*RF
+    DROMG=MAX(ABS(OMG)*VV,TT)
+
+    IF(INITIAL_DRV.EQ.0) THEN
+       DRXP=MAX(dels*VV,TT)
+       DRYP=MAX(dels*VV,TT)
+       DRZP=MAX(dels*VV,TT)
+       delk=SQRT(RKXP**2+RKYP**2+RKZP**2)
+       DRKXP=MAX(delk*VV,TT)
+       DRKYP=MAX(delk*VV,TT)
+       DRKZP=MAX(delk*VV,TT)
+       INITIAL_DRV=1
+    ELSE
+       DRXP=MAX(DXP_save*VV,TT)
+       DRYP=MAX(DYP_save*VV,TT)
+       DRZP=MAX(DZP_save*VV,TT)
+       DRKXP=MAX(DKXP_save*VV,TT)
+       DRKYP=MAX(DKYP_save*VV,TT)
+       DRKZP=MAX(DKZP_save*VV,TT)
+    END IF
+
+    DOMG=(DISPXR(XP,YP,ZP,RKXP,RKYP,RKZP,OMG+DROMG) &
+         -DISPXR(XP,YP,ZP,RKXP,RKYP,RKZP,OMG-DROMG))/(2.D0*DROMG)
+    DXP =(DISPXR(XP+DRXP,YP,ZP,RKXP,RKYP,RKZP,OMG) &
+         -DISPXR(XP-DRXP,YP,ZP,RKXP,RKYP,RKZP,OMG))/(2.D0*DRXP)
+    DYP =(DISPXR(XP,YP+DRYP,ZP,RKXP,RKYP,RKZP,OMG) &
+         -DISPXR(XP,YP-DRYP,ZP,RKXP,RKYP,RKZP,OMG))/(2.D0*DRYP)
+    DZP =(DISPXR(XP,YP,ZP+DRZP,RKXP,RKYP,RKZP,OMG) &
+         -DISPXR(XP,YP,ZP-DRZP,RKXP,RKYP,RKZP,OMG))/(2.D0*DRZP)
+    DKXP=(DISPXR(XP,YP,ZP,RKXP+DRKXP,RKYP,RKZP,OMG) &
+         -DISPXR(XP,YP,ZP,RKXP-DRKXP,RKYP,RKZP,OMG))/(2.D0*DRKXP)
+    DKYP=(DISPXR(XP,YP,ZP,RKXP,RKYP+DRKYP,RKZP,OMG) &
+         -DISPXR(XP,YP,ZP,RKXP,RKYP-DRKYP,RKZP,OMG))/(2.D0*DRKYP)
+    DKZP=(DISPXR(XP,YP,ZP,RKXP,RKYP,RKZP+DRKZP,OMG) &
+         -DISPXR(XP,YP,ZP,RKXP,RKYP,RKZP-DRKZP,OMG))/(2.D0*DRKZP)
+
+    SELECT CASE(MDLWRF)
+    CASE(0)
+       IF(DOMG.GT.0.D0) THEN
+          DS= SQRT(DKXP**2+DKYP**2+DKZP**2)
+       ELSE
+          DS=-SQRT(DKXP**2+DKYP**2+DKZP**2)
+       ENDIF
+    CASE(1)
+       DS=DOMG
+    END SELECT
+
+    VX   =-DKXP/DS
+    VY   =-DKYP/DS
+    VZ   =-DKZP/DS
+
+    VKX  = DXP/DS
+    VKY  = DYP/DS
+    VKZ  = DZP/DS
+
+    VDU  =-2.D0*ABS(DISPXI(XP,YP,ZP,RKXP,RKYP,RKZP,OMG)/DS)
+
+    F(1)=VX
+    F(2)=VY
+    F(3)=VZ
+    F(4)=VKX
+    F(5)=VKY
+    F(6)=VKZ
+    IF(UU.LT.0.D0) THEN
+       F(7)=0.D0
+    ELSE
+       F(7)=VDU*UU 
+    ENDIF
+
+    DXP_save=VX*DS
+    DYP_save=VY*DS
+    DZP_save=VZ*DS
+    DKXP_save=VKX*DS
+    DKYP_save=VKY*DS
+    DKZP_save=VKZ*DS
+
+    IF(idebug_wr(12).NE.0) THEN
+       WRITE(26,'(A)') '*** idebug_wr(12): wrfdrv'
+       WRITE(26,'(A,4ES12.4)') 'x7ds:',X,Y(7),F(7),DS
+       WRITE(26,'(A,6ES12.4)') 'y   :',Y(1),Y(2),Y(3),Y(4),Y(5),Y(6)
+       WRITE(26,'(A,6ES12.4)') 'f   :',F(1),F(2),F(3),F(4),F(5),F(6)
+    END IF
+    RETURN
+  END SUBROUTINE WRFDRV
+
+!  --- slave routine for ray tracing ---
+
+!  Y(1)=X
+!  Y(2)=Y
+!  Y(3)=Z
+!  Y(4)=RKX
+!  Y(5)=RKY
+!  Y(6)=RKZ
+!  Y(7)=W
+
+  SUBROUTINE WRFDRV_ORG(X,Y,F)
+
+    USE wrcomm
+    USE wrsub,ONLY: DISPXR,DISPXI
+    IMPLICIT NONE
+    REAL(rkind),INTENT(IN):: X,Y(NEQ)
+    REAL(rkind),INTENT(OUT):: F(7)
+    REAL(rkind):: VV,TT,RF,XP,YP,ZP,RKXP,RKYP,RKZP,UU,OMG,ROMG
     REAL(rkind):: RXP,RYP,RZP,RRKXP,RRKYP,RRKZP
     REAL(rkind):: DOMG,DXP,DYP,DZP,DKXP,DKYP,DKZP,DS
     REAL(rkind):: VX,VY,VZ,VKX,VKY,VKZ,VDU
@@ -1118,7 +1242,7 @@ CONTAINS
          WRITE(26,'(A,6ES12.4)') 'f   :',F(1),F(2),F(3),F(4),F(5),F(6)
       END IF
       RETURN
-  END SUBROUTINE WRFDRV
+  END SUBROUTINE WRFDRV_ORG
 
 !  --- slave routine for symplectic method ---
 
